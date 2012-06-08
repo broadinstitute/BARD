67270ea93c2eb70ce95ceef8c72d2b32e5efccb6
Mark Southern <mrsouthern@hotmail.com>
2012-06-08 14:11:44 -0400

    seeing getFlushMode is not valid without active transaction

--- Data_Migration/GrailsDataMig/src/groovy/bard/db/util/Inserter.groovy
+++ Data_Migration/GrailsDataMig/src/groovy/bard/db/util/Inserter.groovy
@@ -4,36 +4,82 @@ import edu.scripps.fl.pubchem.PubChemFactory
 import edu.scripps.fl.pubchem.db.PCAssay
 import edu.scripps.fl.pubchem.PubChemXMLParserFactory
 import bard.db.registration.Assay
+import bard.db.experiment.Experiment
+import bard.db.registration.ExternalAssay
+import bard.db.registration.ExternalSystem
 
-/**
- * Created by IntelliJ IDEA.
- * User: balexand
- * Date: 5/23/12
- * Time: 2:32 PM
- * To change this template use File | Settings | File Templates.
- */
 class Inserter {
     Inserter(){}
+
+    void runMLPInsert() {
+        Set<Long> mlpAids = PubChemFactory.getInstance().getMolecularLibrariesOrBeforeAIDs()
+        for(Long aid: mlpAids)
+            runInsert(aid)
+    }
+
+/*    void runInsertTest(long assayId) {
+        Assay bardAssay = new Assay()
+        bardAssay.setAssayName("test")
+        bardAssay.setAssayVersion("1")
+        bardAssay.setVersion((pcAssay.version as String)
+        bardAssay.setDateCreated(new Date())
+        bardAssay.setDescription("description")
+        bardAssay.setAssayStatus("Active")
+        bardAssay.setReadyForExtraction('Ready')
+        bardAssay.save()
+    } */
+
     void runInsert(long assayId ){
         InputStream is = PubChemFactory.getInstance().getXmlDescr(assayId)
         List<PCAssay> assays = PubChemXMLParserFactory.getInstance().populateAssayFromXML(is, false)
         PCAssay pcAssay = assays.get(0)
-        println assays.get(0).getExtRegId()
+//        String.format("%s.%s", pcAssay.getVersion(), pcAssay.getRevision())
+        Experiment expt = new Experiment()
+        expt.setLastUpdated(pcAssay.getModifyDate())
 
+        // id, status, name, assay_version, version, date_created, ready_for_extraction
         Assay bardAssay = new Assay()
-//        AssayStatus assayStatus =  AssayStatus.findByStatusLike("%Pending")
-        bardAssay.setAssayName(pcAssay.getName().substring(0,127))
-        bardAssay.setAssayVersion(pcAssay.version as String )
+        bardAssay.setAssayName(pcAssay.getName())
+        bardAssay.setAssayVersion(pcAssay.version as String)
+        bardAssay.setVersion(1)
         bardAssay.setDateCreated(pcAssay.getDepositDate() )
-//        bardAssay.assayStatus =   assayStatus
-        if ( !bardAssay.save( flush: true ) ) {
-            bardAssay.errors.each {
+        bardAssay.setDescription(pcAssay.getDescription())
+        bardAssay.setAssayStatus("Active")
+        bardAssay.setReadyForExtraction('Ready')
+
+ //       ExternalSystem system =  ExternalSystem.findBySystemNameLike("PubChem")
+
+        try {
+            if ( !bardAssay.save( /* flush: true */ ) ) {
+                bardAssay.errors.each {
+                    println "Problem saving record = ${it}"
+                }
+                print 'hello'
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace()
+        }
+
+
+
+        ExternalAssay extAssay = new ExternalAssay()
+        extAssay.setExternalSystem(system)
+        extAssay.setAssay(bardAssay)
+        extAssay.setDateCreated(pcAssay.getDepositDate())
+        extAssay.setExtAssayId("aid=" + pcAssay.getAID())
+        extAssay.setVersion(pcAssay.getVersion())
+
+
+
+        if ( !extAssay.save( flush: true ) ) {
+            extAssay.errors.each {
                 println "Problem saving record = ${it}"
             }
-
-
         }
-        println "finished saving record."
+
+
+        println "finished saving AID" + assayId
     }
 
 }
