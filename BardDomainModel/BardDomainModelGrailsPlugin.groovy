35253a6b98b554498318708c5bf94b9582208358
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-06-12 09:05:15 -0400

    Adding status for some of the entities

--- BardDomainModel/BardDomainModelGrailsPlugin.groovy
+++ BardDomainModel/BardDomainModelGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class BardDomainModelGrailsPlugin {
     String groupId = 'org.grails.plugins'
 
     // the plugin version
-    def version = "0.1.1"
+    def version = "0.1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
