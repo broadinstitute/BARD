3babf3dfc22e0ff98507312487dbbca680b51e3a
unknown <balexand@merlin.(none)>
2012-06-17 10:13:50 -0400

    add some tests to the domain model plugin

--- BardDomainModel/test/unit/bard/db/dictionary/AssayDescriptorTests.groovy
+++ BardDomainModel/test/unit/bard/db/dictionary/AssayDescriptorTests.groovy
@@ -1,17 +1,62 @@
 package bard.db.dictionary
 
-
-
-import grails.test.mixin.*
-import org.junit.*
+import grails.test.GrailsUnitTestCase
+import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.test.mixin.TestMixin
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(AssayDescriptor)
-class AssayDescriptorTests {
+@TestMixin(GrailsUnitTestMixin)
+class AssayDescriptorTests extends GrailsUnitTestCase {
+
+    AssayDescriptor assayDescriptor
+
+    protected void setUp() {
+        super.setUp()
+        assayDescriptor = new AssayDescriptor(parent: null,
+                element: new Element(),
+                label: 'label',
+                description: null,
+                abbreviation: null,
+                acronym: null,
+                synonyms: null,
+                externalURL: null,
+                unit: null,
+                elementStatus: "Pending")
+
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testValidConstraints() {
+        mockForConstraintsTests( AssayDescriptor )
+
+        // example of valid object
+        assertTrue "Valid AssayDescriptor", assayDescriptor.validate()
+    }
+
+    void testInvalidConstraints1() {
+        mockForConstraintsTests( AssayDescriptor )
+
+        assayDescriptor.elementStatus = null
+        assertFalse "Missing elementStatus should make AssayDescriptor invalid", assayDescriptor.validate()
+        assertEquals "nullable", assayDescriptor.errors.elementStatus
+    }
+
+
+    void testInvalidConstraints2() {
+        mockForConstraintsTests( AssayDescriptor )
+
+        assayDescriptor.element = null
+        assertFalse "Missing element should make AssayDescriptor invalid", assayDescriptor.validate()
+        assertEquals "nullable", assayDescriptor.errors.element
+    }
+
 
     void testSomething() {
-       fail "Implement me"
+        assertTrue(true)
     }
 }
---
+++ BardDomainModel/test/unit/bard/db/dictionary/BiologyDescriptorTests.groovy
@@ -0,0 +1,60 @@
+package bard.db.dictionary
+
+
+import grails.test.GrailsUnitTestCase
+import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.test.mixin.TestMixin
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: balexand
+ * Date: 6/16/12
+ * Time: 2:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@TestMixin(GrailsUnitTestMixin)
+class BiologyDescriptorTests extends GrailsUnitTestCase {
+
+    BiologyDescriptor biologyDescriptor
+
+    protected void setUp() {
+        super.setUp()
+        biologyDescriptor = new BiologyDescriptor(parent: null,
+                element: new Element(),
+                label: 'label',
+                description: null,
+                abbreviation: null,
+                acronym: null,
+                synonyms: null,
+                externalURL: null,
+                unit: null,
+                elementStatus: "Pending")
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testValidConstraints() {
+        mockForConstraintsTests( BiologyDescriptor )
+
+        assertTrue "Valid AssayDescriptor", biologyDescriptor.validate()
+    }
+
+    void testInvalidConstraints1() {
+        mockForConstraintsTests( BiologyDescriptor )
+
+        biologyDescriptor.elementStatus = null
+        assertFalse "Missing elementStatus should make biologyDescriptor invalid", biologyDescriptor.validate()
+        assertEquals "nullable", biologyDescriptor.errors.elementStatus
+    }
+
+    void testInvalidConstraints2() {
+        mockForConstraintsTests( BiologyDescriptor )
+
+        biologyDescriptor.element = null
+        assertFalse "Missing element should make biologyDescriptor invalid", biologyDescriptor.validate()
+        assertEquals "nullable", biologyDescriptor.errors.element
+    }
+
+}
