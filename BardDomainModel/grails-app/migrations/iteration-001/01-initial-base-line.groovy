442edfd4ca9a15f547a304f19f45f982918ce627
Dan Durkin <ddurkin@broadinstitute.org>
2012-06-18 15:29:42 -0400

    added some initial assay data

--- BardDomainModel/grails-app/migrations/iteration-001/01-initial-base-line.groovy
+++ BardDomainModel/grails-app/migrations/iteration-001/01-initial-base-line.groovy
@@ -1,24 +1,15 @@
-databaseChangeLog = {
-    changeSet(author: "ddurkin", id: "01-oracle-ddl-baseline.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/01-oracle-ddl-baseline.sql')
-    }
-    changeSet(author: "ddurkin", id: "02-element-inserts.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/02-element-inserts.sql')
-    }
-    changeSet(author: "ddurkin", id: "03-element-inserts.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/03-element-inserts.sql')
-    }
-    changeSet(author: "ddurkin", id: "04-element_hierarchy-inserts.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/04-element_hierarchy-inserts.sql')
-    }
-    changeSet(author: "ddurkin", id: "05-ontology-insert.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/05-ontology-insert.sql')
-    }
-    changeSet(author: "ddurkin", id: "06-ontology_item-inserts.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/06-ontology_item-inserts.sql')
-    }
-    changeSet(author: "ddurkin", id: "07-tree_root-inserts.sql", dbms:'oracle') {
-        sqlFile(path: 'iteration-001/sql/07-tree_root-inserts.sql')
-    }
+import static groovy.io.FileType.FILES
 
+
+databaseChangeLog = {
+    File migrationsDir = new File('grails-app/migrations')
+    String iteration001SqlRelativeDir = 'iteration-001/sql'
+    File iteration001SqlDir = new File(migrationsDir, iteration001SqlRelativeDir)
+    iteration001SqlDir.eachFileMatch(FILES, ~/\d+.*.sql/) {file ->
+
+        String fileName = file.name
+        changeSet(author: "ddurkin", id: fileName, dbms: 'oracle') {
+            sqlFile(path: "${iteration001SqlRelativeDir}/${fileName}")
+        }
+    }
 }
---
+++ BardDomainModel/grails-app/migrations/iteration-001/sql/13-external_assay-inserts.sql
@@ -0,0 +1,3 @@
+insert into external_assay (external_system_id, assay_id, ext_assay_id) values (1, 1, 'aid=644');
+insert into external_assay (external_system_id, assay_id, ext_assay_id) values (1, 2, 'aid=504633');
+insert into external_assay (external_system_id, assay_id, ext_assay_id) values (1, 3, 'aid=602305');
