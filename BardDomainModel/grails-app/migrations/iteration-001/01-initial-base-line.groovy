bb1cb918d4612df160433fe3dc737a90619368ca
Dan Durkin <ddurkin@broadinstitute.org>
2012-06-18 15:48:29 -0400

    added schema to datasource for testing

--- BardDomainModel/grails-app/migrations/iteration-001/01-initial-base-line.groovy
+++ BardDomainModel/grails-app/migrations/iteration-001/01-initial-base-line.groovy
@@ -6,7 +6,6 @@ databaseChangeLog = {
     String iteration001SqlRelativeDir = 'iteration-001/sql'
     File iteration001SqlDir = new File(migrationsDir, iteration001SqlRelativeDir)
     iteration001SqlDir.eachFileMatch(FILES, ~/\d+.*.sql/) {file ->
-
         String fileName = file.name
         changeSet(author: "ddurkin", id: fileName, dbms: 'oracle') {
             sqlFile(path: "${iteration001SqlRelativeDir}/${fileName}")
