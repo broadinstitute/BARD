b9ca27d6b6022ee90d2df618f13859677510ef2f
gwalzer <gwalzer@broadinstitute.org>
2012-06-22 14:07:21 -0400

    user story 31623595: As I developer, i would like to change the bardDataExportSample module name to dataExport

---
+++ dataExport/grails-app/conf/spring/resources.groovy
@@ -0,0 +1,27 @@
+import dataexport.registration.AssayDefinitionMediaTypesDTO
+
+// Place your Spring DSL code here
+beans = {
+    final String elementMediaType=grailsApplication.config.bard.data.export.dictionary.element.xml
+    //inject element mime type here
+    dictionaryExportHelperService(dataexport.dictionary.DictionaryExportHelperService, elementMediaType) {
+        dataSource = ref('dataSource')
+        grailsLinkGenerator = ref('grailsLinkGenerator')
+    }
+    String dictionaryMediaType = grailsApplication.config.bard.data.export.dictionary.xml
+    rootService(dataexport.util.RootService, dictionaryMediaType) {
+        grailsLinkGenerator = ref('grailsLinkGenerator')
+    }
+    final String resultTypeMediaType=grailsApplication.config.bard.data.export.dictionary.resultType.xml
+    final String assayMediaType=grailsApplication.config.bard.data.export.assay.xml
+    final String assayDocMediaType=grailsApplication.config.bard.data.export.assay.doc.xml
+    final String assaysMediaType=grailsApplication.config.bard.data.export.assays.xml
+
+    AssayDefinitionMediaTypesDTO dto =  new AssayDefinitionMediaTypesDTO( elementMediaType,resultTypeMediaType,
+           assayMediaType,assayDocMediaType,
+          assaysMediaType)
+    assayExportHelperService(dataexport.registration.AssayExportHelperService,dto){
+        dataSource = ref('dataSource')
+        grailsLinkGenerator = ref('grailsLinkGenerator')
+    }
+}
