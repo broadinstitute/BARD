94e0bbf72ec42853f9c7b847e9c9e3c77890b6bb
gwalzer <gwalzer@broadinstitute.org>
2012-07-13 14:50:11 -0400

    updating all JSONObject type to be wslite.json.JSONObject: republishing ElasticSearchPlugin; updating bardqueryapi and BARD to use the change

--- bardqueryapi/test/unit/bardqueryapi/BardWebInterfaceControllerUnitSpec.groovy
+++ bardqueryapi/test/unit/bardqueryapi/BardWebInterfaceControllerUnitSpec.groovy
@@ -3,10 +3,9 @@ package bardqueryapi
 import elasticsearchplugin.ESAssay
 import elasticsearchplugin.ESCompound
 import elasticsearchplugin.ElasticSearchService
-import grails.converters.JSON
 import grails.test.mixin.TestFor
-import org.codehaus.groovy.grails.web.json.JSONObject
 import spock.lang.Specification
+import wslite.json.JSONObject
 
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
@@ -84,9 +83,9 @@ class BardWebInterfaceControllerUnitSpec extends Specification {
         872 == model.compoundId
 
         where:
-        label                                | cid              | compoundJson                     | expectedCompoundJson
-        "compound not found - error message" | new Integer(872) | [:] as JSONObject                | '{}'
-        "Return a compound"                  | new Integer(872) | JSON.parse(compoundDocumentJson) | '{"probeId":"null","sids":"[4243156,24368917]","cid":"3237916","smiles":"C-C"}'
+        label                                | cid              | compoundJson                         | expectedCompoundJson
+        "compound not found - error message" | new Integer(872) | new JSONObject()                     | '{}'
+        "Return a compound"                  | new Integer(872) | new JSONObject(compoundDocumentJson) | '{"probeId":"null","sids":[4243156,24368917],"smiles":"C-C","cid":"3237916"}'
     }
 
     void "test search #label"() {
@@ -106,9 +105,9 @@ class BardWebInterfaceControllerUnitSpec extends Specification {
         assert model.projects == []
 
         where:
-        label                                | searchTerm | resultJson                                                 | expectedTotalCompounds | expectedAssays | expectedCompounds
-        "nothing was found"                  | '644'      | [assays: [], compounds: []] as JSONObject                  | 0                      | 0              | 0
-        "An Assay and a compound were found" | '644'      | [assays: [esAssay], compounds: [esCompound]] as JSONObject | 1                      | 1              | 1
+        label                                | searchTerm | resultJson                                   | expectedTotalCompounds | expectedAssays | expectedCompounds
+        "nothing was found"                  | '644'      | [assays: [], compounds: []]                  | 0                      | 0              | 0
+        "An Assay and a compound were found" | '644'      | [assays: [esAssay], compounds: [esCompound]] | 1                      | 1              | 1
     }
 
     void "test autocomplete #label"() {
@@ -123,9 +122,9 @@ class BardWebInterfaceControllerUnitSpec extends Specification {
         assertEquals expectedRender, controller.response.json.toString()
 
         where:
-        label                 | searchString | expectedJSONResponse                            | expectedRender
-        "Return two strings"  | "Bro"        | new wslite.json.JSONObject(AUTO_COMPLETE_NAMES) | '["Broad Institute MLPCN Platelet Activation"]'
-        "Return Empty String" | "644"        | []                                              | '[""]'
+        label                 | searchString | expectedJSONResponse                | expectedRender
+        "Return two strings"  | "Bro"        | new JSONObject(AUTO_COMPLETE_NAMES) | '["Broad Institute MLPCN Platelet Activation"]'
+        "Return Empty String" | "644"        | []                                  | '[""]'
 
     }
 
@@ -141,9 +140,9 @@ class BardWebInterfaceControllerUnitSpec extends Specification {
         responseList == expectedList
 
         where:
-        label                 | searchString | expectedJSONResponse                            | expectedList
-        "Return two strings"  | "Bro"        | new wslite.json.JSONObject(AUTO_COMPLETE_NAMES) | ["Broad Institute MLPCN Platelet Activation"]
-        "Return Empty String" | "644"        | []                                              | []
+        label                 | searchString | expectedJSONResponse                | expectedList
+        "Return two strings"  | "Bro"        | new JSONObject(AUTO_COMPLETE_NAMES) | ["Broad Institute MLPCN Platelet Activation"]
+        "Return Empty String" | "644"        | []                                  | []
 
     }
 }
