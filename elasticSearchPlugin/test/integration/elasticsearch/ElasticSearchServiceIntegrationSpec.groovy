c5365736fef9ae90df4d4999233d54c3cc16053e
Ben R Alexander <balexand@broadinstitute.org>
2012-07-17 15:11:46 +0100

    modded integration search

--- elasticSearchPlugin/test/integration/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
+++ elasticSearchPlugin/test/integration/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
@@ -4,16 +4,25 @@ import elasticsearchplugin.ElasticSearchService
 import grails.plugin.spock.IntegrationSpec
 import wslite.json.JSONObject
 
-/**
- * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+/*
+ *
  */
 class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
 
+    //This is the service we are here to test
     ElasticSearchService  elasticSearchService
+
+    String elasticSearchRoot
+    String elasticAssayIndex
+    String elasticCompoundIndex
+    String elasticSearchRequester
 
 
     void setup() {
-        // Setup logic here
+        elasticSearchRoot =  "http://bard-dev-vm:9200"
+        elasticAssayIndex =  "/assays"
+        elasticCompoundIndex =  "/compounds"
+        elasticSearchRequester  =  "/_search"
     }
 
     void tearDown() {
@@ -21,24 +30,23 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
     }
 
 
-    void testElasticSearchServiceQry() {
+
+    void testElasticSearch_SingleAID_AllQry_AssayIndex() {
 
         given:
         elasticSearchService != null
+        String elasticNodeSpecifier =  elasticSearchRoot +   elasticAssayIndex +   elasticSearchRequester
         String testStr = """
-{"query":{"bool":{"must":[{"query_string":{"default_field":"_all","query":"644"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}"""
-        String elasticSearchNode =  "http://bard-dev-vm:9200/assays/_search"
+{"query":{"bool":{"must":[{"query_string":{"default_field":"_all","query":"174"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}"""
+
 
         when:
 
         final JSONObject jsonObject =  new JSONObject(testStr.toString())
-        def returnJson =   elasticSearchService.searchQueryStringQuery(elasticSearchNode,jsonObject)
-//        File fk = new File("f.f")
+        def returnJson =   elasticSearchService.searchQueryStringQuery(elasticNodeSpecifier,jsonObject)
 
         then:
 
-//        fk << "returnJson.getClass().name=${returnJson.getClass().name}" +
-//                "returnJson=${returnJson.toString()}"
         // demonstrate that something came back
         assert returnJson
 
@@ -55,32 +63,117 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         assert jsonHitObject["hits"][1]._type=="assay"
 
     }
+
+
+
+
+
+
+    void testElasticSearch_AllQry_AssayIndex() {
+
+        given:
+        elasticSearchService != null
+        String elasticNodeSpecifier =  elasticSearchRoot +   elasticAssayIndex +   elasticSearchRequester
+        String testStr = """
+{"query":{"bool":{"must":[{"query_string":{"default_field":"_all","query":"644"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}"""
+
+
+        when:
+
+        final JSONObject jsonObject =  new JSONObject(testStr.toString())
+        def returnJson =   elasticSearchService.searchQueryStringQuery(elasticNodeSpecifier,jsonObject)
+
+        then:
+
+        // demonstrate that something came back
+        assert returnJson
+
+        // the Json starts off the right way
+        assert returnJson.containsKey("hits")
+
+        // we need back a couple of different types
+        JSONObject jsonHitObject = returnJson["hits"]
+        assert jsonHitObject.containsKey("total")
+
+        // now let's look at the real contents
+        assert jsonHitObject["hits"].size()==2
+        assert jsonHitObject["hits"][0]._type=="compound"
+        assert jsonHitObject["hits"][1]._type=="assay"
+
+    }
+
+
+
+
+
+    void testElasticSearch_SingleCID_AllQry_AssayIndex() {
+
+        given:
+        elasticSearchService != null
+        String elasticNodeSpecifier =  elasticSearchRoot +   elasticCompoundIndex +   elasticSearchRequester
+        String testStr = """
+{"query":{"bool":{"must":[{"query_string":{"default_field":"_all","query":"174"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}"""
+
+
+        when:
+
+        final JSONObject jsonObject =  new JSONObject(testStr.toString())
+        def returnJson =   elasticSearchService.searchQueryStringQuery(elasticNodeSpecifier,jsonObject)
+
+        then:
+
+        // demonstrate that something came back
+        assert returnJson
+
+        // the Json starts off the right way
+        assert returnJson.containsKey("hits")
+
+        // we need back a couple of different types
+        JSONObject jsonHitObject = returnJson["hits"]
+        assert jsonHitObject.containsKey("total")
+//
+//        // now let's look at the real contents
+        assert jsonHitObject["hits"].size()==1
+        assert jsonHitObject["hits"][0]._type=="compound"
+        assert jsonHitObject["hits"][0]._id=="174"
+
+    }
+
+
+
+
+
+    void testElasticSearch_MultipleCID_AllQry_CompoundsIndex() {
+
+        given:
+        elasticSearchService != null
+        String elasticNodeSpecifier =  elasticSearchRoot +   elasticCompoundIndex +   elasticSearchRequester
+        String testStr = """
+{"query":{"bool":{"must":[{"query_string":{"default_field":"_all","query":"174 3237916"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}"""
+
+
+        when:
+
+        final JSONObject jsonObject =  new JSONObject(testStr.toString())
+        def returnJson =   elasticSearchService.searchQueryStringQuery(elasticNodeSpecifier,jsonObject)
+
+        then:
+
+        // demonstrate that something came back
+        assert returnJson
+
+        // we need back a couple of different types
+        assert returnJson.containsKey("hits")
+        JSONObject jsonHitObject = returnJson["hits"]
+        assert jsonHitObject.containsKey("total")
+
+        // now let's look at the real contents
+        assert jsonHitObject["hits"].size() > 0
+
+    }
+
+
+
+
 }
 
-  /*
-
-
-
-
-/*        given:
-elasticSearchService != null
-
-when:
-def foo ='bar'
-then:
-1==1
-String elasticSearchNode =  "http://bard-dev-vm:9200/assays/_search"
-String testStr = """ {
-"fields": ["name"],
-"query": {
-"query_string": {
-"default_field": "name",
-"query": "644*"
-}
-},
-"size": 10
-}"""
-final JSONObject jsonObject =  new JSONObject(testStr.toString())
-def f = elasticSearchService.searchQueryStringQuery(elasticSearchNode,jsonObject)
-println f.toString()toString
-*/
