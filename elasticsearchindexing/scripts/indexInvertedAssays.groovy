65252e27f9806b5281433fc0c3f851465af4c653
Ben R Alexander <balexand@broadinstitute.org>
2012-07-23 17:29:00 +0100

    update new indexer to include sids, probeids, etc

--- elasticsearchindexing/scripts/indexInvertedAssays.groovy
+++ elasticsearchindexing/scripts/indexInvertedAssays.groovy
@@ -17,8 +17,8 @@ RestClientFactoryService restClientFactoryService = ctx.getBean("restClientFacto
 assert restClientFactoryService
 
 //the name of the elasticsearch index for assays
-String indexName = "invertedassays"
-String indexType = "compond"
+String indexName = "compound"
+String indexType = "compound"
 
 //url to root of NCGC REST API
 final String ncgcRootURL = elasticSearchCompoundsIndexService.ncgcRootURL
@@ -44,62 +44,89 @@ RESTClient cloneRestClient = restClientFactoryService.createNewRestClient("${ela
 
 
 // get a list of cids from elsatic search
-List<String> cids
+/*List<String> cids
 try {
- cids = elasticSearchCompoundsIndexService.fetchAllUniqueCIDsFromElasticSearch()
+    cids = elasticSearchCompoundsIndexService.fetchAllUniqueCIDsFromElasticSearch()
 } catch (Exception eee) {
     println eee.message
 }
 
 println "fetched ${cids.size()} cids.  Now process them one at a time."
-
-//File f = new File("e:/Temp/compounds.txt")
+*/
+//File f = new File("c:/Temp/compounds.txt")
 //for (String cid in cids) {
-//  f<<"${cid}\n"
+//    f<<"${cid}\n"
 //}
-File f = new File("e:/Temp/error.txt")
-//File f = new File("e:/Temp/compounds.txt")
+//f.flush()
+//File f = new File("e:/Temp/error.txt")
+File errorfile = new File("c:/Temp/err.txt")
+File infile = new File("c:/Temp/cmpdhead.txt")
+
+println "processing the file = ${infile.name}"
+
+
+
+
 
 // now cycle over every compound
-for (String cid in cids) {
-   if (!("0".equals(cid.trim()) ))
-   try {
-      // compose a URL and query ncgc
-      cloneRestClient.url = ncgcCompoundsURL+cid+"/exptdata"
-      wslite.json.JSONObject jsonObject = elasticSearchCompoundsIndexService.executeGetRequestJSON(cloneRestClient)
-      assert jsonObject
-      
-      // take apart the respose to get the assay ids
-      wslite.json.JSONArray jsonArray=jsonObject["collection"]
-      def combinedAids = new  StringBuffer()
-      for (def exptURL in jsonArray) {
-//      def exptNumber = exptURL.toString().replaceAll('/bard/rest/v1/exptdata/', '')
-         combinedAids <<   exptURL.toString().replaceAll('/bard/rest/v1/exptdata/', '') + ","
-      }
-      // remove last character ( a comma)
-     combinedAids = combinedAids.substring(0,combinedAids.size()-1)
-     
-      // insert into elastic search index
-      final String cidJson = "{cid: ${cid}, apids:[${combinedAids.toString()}]}"
-      final  wslite.json.JSONObject requestToSend = new wslite.json.JSONObject(cidJson)
-                
-      cloneRestClient.url = "${elasticSearchURL}${indexName}/${indexType}"
-      elasticSearchCompoundsIndexService.postRequest( cloneRestClient,requestToSend.toString() )
-  
-    } catch (Exception ee) {
-       ee.printStackTrace()
-       println ee.message
-       f << "failed on ${cid}\n"
-   }   
-}
+infile.eachLine{cid->
+//for (String cid in cids) {
 
+
+    if (!("0".equals(cid.trim()) ))
+        try {
+            // compose a URL and query ncgc for assay IDS
+            cloneRestClient.url = ncgcCompoundsURL+cid+"/experiments"
+            def combinedAids = new  StringBuffer()
+            boolean stillMoreToDo=true
+            while (stillMoreToDo) {
+                wslite.json.JSONObject jsonObject = elasticSearchCompoundsIndexService.executeGetRequestJSON(cloneRestClient)
+                assert jsonObject
+                // take apart the respose to get the assay ids
+                wslite.json.JSONArray jsonArray=jsonObject."collection"
+                for (def exptURL in jsonArray) {
+                    combinedAids <<   exptURL.toString().replaceAll('/bard/rest/v1/experiments/', '') + ","
+                }
+                // do we have more?
+                def link=jsonObject."link"
+                stillMoreToDo = ((!wslite.json.JSONObject.NULL.equals(link)) )
+                if (stillMoreToDo) {
+                    cloneRestClient.url = "${ncgcRootURL.substring(0,ncgcRootURL.size()-1)}${link.toString()}"
+                }
+            }
+            // remove last character ( a comma)
+            combinedAids = combinedAids.substring(0,combinedAids.size()-1)
+
+            // compose a URL and query ncgc for compounds specific
+            cloneRestClient.url = ncgcCompoundsURL+cid
+            jsonObject = elasticSearchCompoundsIndexService.executeGetRequestJSON(cloneRestClient)
+            assert jsonObject
+            // take apart the respose to get the assay ids
+            wslite.json.JSONArray sidsArray=jsonObject."sids"
+            def probeId=jsonObject."probeId"
+            def url=jsonObject."url"
+            def smiles=jsonObject."smiles"
+            // insert into elastic search index
+            final String cidJson = "{smiles: \"${smiles.toString()}\",url: ${url.toString()}, apids:[${combinedAids.toString()}],sids:${sidsArray.toString()}, probeId:${probeId.toString ()},cid: ${cid}}"
+
+            cloneRestClient.url = "${elasticSearchURL}${indexName}/${indexType}"
+            elasticSearchCompoundsIndexService.postRequest( cloneRestClient,cidJson )
+
+        } catch (Exception ee) {
+            ee.printStackTrace()
+            println ee.message
+            errorfile << "failed on ${cid}\n"
+        }
+}
 // finally, send a refresh so that everything is ready to go
+
 try{
-   cloneRestClient.url = "${elasticSearchURL}${indexName}/_refresh"
-   elasticSearchCompoundsIndexService.postRequest( cloneRestClient,"" )
+    cloneRestClient.url = "${elasticSearchURL}${indexName}/_refresh"
+    elasticSearchCompoundsIndexService.postRequest( cloneRestClient,"" )
 
 } catch (Exception ee) {
     ee.printStackTrace()
     println ee.message
-}   
+}
+
 
