6fe600e2b0904a8372852d56b55952c96e96da61
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-07-25 07:51:15 -0400

    published new version of plugin

--- elasticSearchPlugin/ElasticSearchGrailsPlugin.groovy
+++ elasticSearchPlugin/ElasticSearchGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class ElasticSearchGrailsPlugin {
     String groupId = 'org.grails.plugins'
 
     // the plugin version
-    def version = "0.3-SNAPSHOT"
+    def version = "0.3.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
