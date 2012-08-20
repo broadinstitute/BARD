99407a03858f98cfd1e1366c5ee13d327d1091ad
Dan Durkin <ddurkin@broadinstitute.org>
2012-08-20 09:16:27 -0400

    added tests for MeasureContext

--- BardDomainModel/grails-app/domain/bard/db/registration/MeasureContext.groovy
+++ BardDomainModel/grails-app/domain/bard/db/registration/MeasureContext.groovy
@@ -2,24 +2,31 @@ package bard.db.registration
 
 class MeasureContext {
 
-	String contextName
-	Date dateCreated
-	Date lastUpdated
-	String modifiedBy
+    private static final int CONTEXT_NAME_MAX_SIZE = 128
+    private static final int MODIFIED_BY_MAX_SIZE = 40
+
+    String contextName
     Assay assay
 
-	static hasMany = [measureContextItems: MeasureContextItem,
-	                  measures: Measure]
+    Set<Measure> measures = [] as Set<Measure>
+    Set<MeasureContextItem> measureContextItems = [] as Set<MeasureContextItem>
 
-	static mapping = {
+    Date dateCreated
+    Date lastUpdated
+    String modifiedBy
+
+    static hasMany = [measureContextItems: MeasureContextItem,
+            measures: Measure]
+
+    static mapping = {
         id(column: "MEASURE_CONTEXT_ID", generator: "sequence", params: [sequence: 'MEASURE_CONTEXT_ID_SEQ'])
-	}
+    }
 
-	static constraints = {
-		contextName maxSize: 128
-        assay nullable: false
-		dateCreated maxSize: 19
-		lastUpdated nullable: true, maxSize: 19
-		modifiedBy nullable: true, maxSize: 40
-	}
+    static constraints = {
+        contextName(maxSize: CONTEXT_NAME_MAX_SIZE, blank: false)
+        assay()
+        dateCreated(nullable: false)
+        lastUpdated(nullable: true)
+        modifiedBy(nullable: true, blank: false, maxSize: MODIFIED_BY_MAX_SIZE)
+    }
 }
---
+++ BardDomainModel/test/integration/bard/db/registration/MeasureContextIntegrationSpec.groovy
@@ -0,0 +1,144 @@
+package bard.db.registration
+
+import org.junit.Before
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static bard.db.registration.MeasureContext.CONTEXT_NAME_MAX_SIZE
+import static bard.db.registration.MeasureContext.MODIFIED_BY_MAX_SIZE
+import static test.TestUtils.assertFieldValidationExpectations
+import static test.TestUtils.createString
+import grails.plugin.spock.IntegrationSpec
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ddurkin
+ * Date: 8/17/12
+ * Time: 2:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Unroll
+class MeasureContextIntegrationSpec extends IntegrationSpec {
+    MeasureContext domainInstance
+
+    @Before
+    void doSetup() {
+        domainInstance = MeasureContext.buildWithoutSave()
+    }
+
+    void "test contextName constraints #desc contextName: '#valueUnderTest'"() {
+        final String field = 'contextName'
+
+        when: 'a value is set for the field under test'
+        domainInstance[(field)] = valueUnderTest
+        domainInstance.validate()
+
+        then: 'verify valid or invalid for expected reason'
+        assertFieldValidationExpectations(domainInstance, field, valid, errorCode)
+
+        and: 'verify the domain can be persisted to the db'
+        if (valid) {
+            domainInstance == domainInstance.save(flush: true)
+        }
+
+        where:
+        desc               | valueUnderTest                          | valid | errorCode
+        'null not valid'   | null                                    | false | 'nullable'
+        'blank not valid'  | ''                                      | false | 'blank'
+        'blank not valid'  | '   '                                   | false | 'blank'
+
+        'too long'         | createString(CONTEXT_NAME_MAX_SIZE + 1) | false | 'maxSize.exceeded'
+        'exactly at limit' | createString(CONTEXT_NAME_MAX_SIZE)     | true  | null
+
+    }
+
+    void "test assay constraints #desc assay: '#valueUnderTest'"() {
+        final String field = 'assay'
+
+        when: 'a value is set for the field under test'
+        domainInstance[(field)] = valueUnderTest
+        domainInstance.validate()
+
+        then: 'verify valid or invalid for expected reason'
+        assertFieldValidationExpectations(domainInstance, field, valid, errorCode)
+
+        and: 'verify the domain can be persisted to the db'
+        if (valid) {
+            domainInstance == domainInstance.save(flush: true)
+        }
+
+        where:
+        desc             | valueUnderTest | valid | errorCode
+        'null not valid' | null           | false | 'nullable'
+        'valid assay'    | Assay.build()  | true  | null
+
+    }
+
+    void "test modifiedBy constraints #desc modifiedBy: '#valueUnderTest'"() {
+
+        final String field = 'modifiedBy'
+
+        when: 'a value is set for the field under test'
+        domainInstance[(field)] = valueUnderTest
+        domainInstance.validate()
+
+        then: 'verify valid or invalid for expected reason'
+        assertFieldValidationExpectations(domainInstance, field, valid, errorCode)
+
+        and: 'verify the domain can be persisted to the db'
+        if (valid) {
+            domainInstance == domainInstance.save(flush: true)
+        }
+
+        where:
+        desc               | valueUnderTest                         | valid | errorCode
+        'too long'         | createString(MODIFIED_BY_MAX_SIZE + 1) | false | 'maxSize.exceeded'
+        'blank valid'      | ''                                     | false | 'blank'
+        'blank valid'      | '  '                                   | false | 'blank'
+
+        'exactly at limit' | createString(MODIFIED_BY_MAX_SIZE)     | true  | null
+        'null valid'       | null                                   | true  | null
+    }
+
+    void "test dateCreated constraints #desc dateCreated: '#valueUnderTest'"() {
+        final String field = 'dateCreated'
+
+        when: 'a value is set for the field under test'
+        domainInstance[(field)] = valueUnderTest
+        domainInstance.validate()
+
+        then: 'verify valid or invalid for expected reason'
+        assertFieldValidationExpectations(domainInstance, field, valid, errorCode)
+
+        and: 'verify the domain can be persisted to the db'
+        if (valid) {
+            domainInstance == domainInstance.save(flush: true)
+        }
+
+        where:
+        desc             | valueUnderTest | valid | errorCode
+        'null not valid' | null           | false | 'nullable'
+        'date valid'     | new Date()     | true  | null
+    }
+
+    void "test lastUpdated constraints #desc lastUpdated: '#valueUnderTest'"() {
+        final String field = 'lastUpdated'
+
+        when: 'a value is set for the field under test'
+        domainInstance[(field)] = valueUnderTest
+        domainInstance.validate()
+
+        then: 'verify valid or invalid for expected reason'
+        assertFieldValidationExpectations(domainInstance, field, valid, errorCode)
+
+        and: 'verify the domain can be persisted to the db'
+        if (valid) {
+            domainInstance == domainInstance.save(flush: true)
+        }
+
+        where:
+        desc         | valueUnderTest | valid | errorCode
+        'null valid' | null           | true  | null
+        'date valid' | new Date()     | true  | null
+    }
+}
