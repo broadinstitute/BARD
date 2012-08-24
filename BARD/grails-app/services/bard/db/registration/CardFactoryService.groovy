327a56711a9c3348e9c21a672e58e4745964cea8
Dan Durkin <ddurkin@broadinstitute.org>
2012-08-24 13:16:37 -0400

    update code for producing card dtos
    this is a work in progress

--- BARD/grails-app/services/bard/db/registration/CardFactoryService.groovy
+++ BARD/grails-app/services/bard/db/registration/CardFactoryService.groovy
@@ -11,63 +11,31 @@ class CardFactoryService {
         if (assay == null || assay.getAssayContexts() == null) {
             return cards
         }
-		
-		
-		
-//        List<AssayContextItem> items = assay.assayContexts.assayContextItems as List
-//        items.removeAll { item -> item.getAssayContext() != null } // TODO add isAssayLevelContextItem() method to domain class
-//        Map<Long, List<AssayContextItem>> grouping = items.groupBy { item ->
-//            if (item.parentGroup != null)
-//                return item.parentGroup.id
-//            else
-//                return item.id
-//        }
-//
-//        List<Long> groupingIds = grouping.keySet() as List
-//        Collections.sort(groupingIds)
-//        for (Long groupingId : groupingIds) {
-//            List<AssayContextItem> itemsInGroup = grouping.get(groupingId)
-//            itemsInGroup = itemsInGroup.sort { a, b -> a.attributeElement?.label <=> b.attributeElement?.label }
-//            CardDto card = new CardDto()
-//            cards.add(card)
-//            for (AssayContextItem item : itemsInGroup) {
-//                if (item.parentGroup == null || item.parentGroup.id == item.id) {
-//                    card.title = generateCardTitle(item)
-//                }
-//                card.lines.add(createCardLineDtoForAssayContextItem(item))
-//            }
-//        }
+
+        for (AssayContext assayContext in assay.assayContexts) {
+            CardDto cardDto = new CardDto()
+            cardDto.title = assayContext.contextName
+            for (AssayContextItem assayContextItem : assayContext.assayContextItems) {
+                cardDto.lines.add(createCardLineDtoForAssayContextItem(assayContextItem))
+            }
+            cards.add(cardDto)
+        }
         return cards
     }
 
-    /**
-     * @param item a parent context item
-     * @return the appropriate title (with special handling for assay component role)
-     */
-    protected String generateCardTitle(MeasureContextItem item) {
-//		if (item.attributeElement.label == ASSAY_COMPONENT_ROLE) {
-//			return item.valueDisplay
-//		}
-//		else {
-//			for (AssayContextItem child : item.getChildren()) {
-//				if (child.attributeElement.label == ASSAY_COMPONENT_ROLE) {
-//					return child.valueDisplay
-//				}
-//			}
-//		}
-		
-		return item.valueDisplay
 
-    }
 
     private CardLineDto createCardLineDtoForAssayContextItem(AssayContextItem item) {
         CardLineDto line = new CardLineDto()
         // TODO change this to call out to Dictionary REST API by adding a DictionaryLookupService
-        line.attributeLabel = item.attributeElement.label
-        line.attributeDefinition = item.attributeElement.description
-        line.valueLabel = item.valueDisplay
-        if (item.valueElement != null) { // TODO add hasControlledVocabularyValue() method to AssayContextItem
-            line.valueDefinition = item.valueElement.description
+        if (item) {
+            line.id = item.id
+            line.attributeLabel = item.attributeElement.label
+            line.attributeDefinition = item.attributeElement.description
+            line.valueLabel = item.valueDisplay
+            if (item.valueElement != null) { // TODO add hasControlledVocabularyValue() method to AssayContextItem
+                line.valueDefinition = item.valueElement.description
+            }
         }
         return line
     }
@@ -83,6 +51,7 @@ class CardLineDto {
     String attributeDefinition;
     String valueLabel;
     String valueDefinition;
+    Long id;
 
     boolean isAttributeDefinitionAvailable() {
         return StringUtils.isNotBlank(attributeDefinition)
