e7d0cdef551f7cec85d9127428811a74323fd012
gwalzer <gwalzer@broadinstitute.org>
2012-09-10 17:57:04 -0400

    fixing bug: https://www.pivotaltracker.com/story/show/35790255

--- web-app/js/search.js
+++ web-app/js/search.js
@@ -13,9 +13,14 @@ $(document).ready(function () {
     $("#searchString").bind("autocompleteselect", function (event, ui) {
         $("#searchButton").click();
     });
-    // make sure to close the autocomplete box when the search button is clicked
+    // make sure to close the autocomplete box when the search button or ENTER are clicked
     $("#searchButton").click(function () {
         $("#searchString").autocomplete("close");
+    });
+    $('#searchButton').keypress(function(eventData) {
+        if(eventData.which == 13) {
+            $("#searchString").autocomplete("close");
+        }
     });
 
 
