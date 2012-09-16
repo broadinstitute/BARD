08fadf6f1b2de3667e64b3430eb458d0223bf8db
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-16 13:48:33 -0400

    Updatings cript

--- jsTests/jsTestDriver.sh
+++ jsTests/jsTestDriver.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+# directory to write output XML (if this doesn't exist, the results will not be generated!)
+OUTPUT_DIR="../target/test-reports"
+mkdir $OUTPUT_DIR
+
+XVFB=`which Xvfb`
+if [ "$?" -eq 1 ];
+then
+    echo "Xvfb not found."
+    exit 1
+fi
+
 FIREFOX=`which firefox`
 if [ "$?" -eq 1 ];
 then
-  echo "Firefox not found."
-  exit 1
+    echo "Firefox not found."
+    exit 1
 fi
-echo $FIREFOX 
+
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
+
+echo $FIREFOX
 
 # run the tests
-java -jar JsTestDriver-1.3.4.b.jar --port 9876 --config jsTestDriver.conf --browser "$FIREFOX" --tests all --testOutput "../target/js-test-reports"
+java -jar JsTestDriver-1.3.4.b.jar --port 9876 --config jsTestDriver.conf --browser "$FIREFOX" --raiseOnFailure --tests all --testOutput "$OUTPUT_DIR"
 RESULT=$?
- 
+
 exit $RESULT
+
+
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+echo "Done."
