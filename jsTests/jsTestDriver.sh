b18a8a13b57a657c76f934e9642fb97150a69dc3
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-17 17:29:23 -0400

    Uncommenting molecular spreadsheet tests

--- jsTests/jsTestDriver.sh
+++ jsTests/jsTestDriver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # directory to write output XML (if this doesn't exist, the results will not be generated!)
-OUTPUT_DIR="test-reports"
+OUTPUT_DIR="../target/test-reports"
 mkdir $OUTPUT_DIR
 
 
@@ -11,13 +11,20 @@ then
     exit 1
 fi
 
-echo $FIREFOX
+#if you executed this script using cygwin, your firefox path would look like
+#/cygdrive/c/Program Files (x86)/Mozilla Firefox/firefox
+#which would not work in this script
+#so we remove the '/cygdrive/c' and repalce it with 'C:'
 
+
+NEW_FIRE_FOX_PATH=${FIREFOX/\/cygdrive\/c/C:};
+echo $FIREFOX
+echo $NEW_FIRE_FOX_PATH
 # run the tests
-java -jar JsTestDriver-1.3.4.b.jar --port 9876 --config jsTestDriver.conf --browser "$FIREFOX" --raiseOnFailure --tests all --testOutput "$OUTPUT_DIR"
-RESULT=$?
+java -jar JsTestDriver-1.3.4.b.jar --port 9876 --config jsTestDriver.conf --browser "$NEW_FIRE_FOX_PATH" --raiseOnFailure --tests all --testOutput "$OUTPUT_DIR"
+#RESULT=$?
 
 
 echo "Done."
 
-exit $RESULT
+#exit $RESULT
--- test/integration/bardqueryapi/MolecularSpreadSheetServiceIntegrationSpec.groovy
+++ test/integration/bardqueryapi/MolecularSpreadSheetServiceIntegrationSpec.groovy
@@ -1,5 +1,8 @@
 package bardqueryapi
 
+import bard.core.Experiment
+import bard.core.ServiceIterator
+import bard.core.Value
 import bard.core.rest.RESTCompoundService
 import bard.core.rest.RESTExperimentService
 import grails.plugin.spock.IntegrationSpec
@@ -31,63 +34,58 @@ class MolecularSpreadSheetServiceIntegrationSpec extends IntegrationSpec {
 
     }
 
-     //TODO: Uncomment once NCGC fix the server issues they are having
-//    void "tests cartAssaysToExperiments #label"() {
-//        given: "That a list of CartAssay objects have been created"
-//        final List<CartAssay> givenCartAssays = cartAssays
-//        when: "We call the cartAssaysToExperiments() with the given list of assay carty objects"
-//        List<Experiment> experiments = molecularSpreadSheetService.cartAssaysToExperiments(givenCartAssays)
-//        then: "We expect experiments for each of the assays to be found"
-//        assert experiments
-//
-//
-//        where:
-//        label                                | cartAssays
-//        "An existing assay with experiments" | [new CartAssay(assayId: new Long(519))]
-//    }
+    void "tests cartAssaysToExperiments #label"() {
+        given: "That a list of CartAssay objects have been created"
+        final List<CartAssay> givenCartAssays = cartAssays
+        when: "We call the cartAssaysToExperiments() with the given list of assay carty objects"
+        List<Experiment> experiments = molecularSpreadSheetService.cartAssaysToExperiments(givenCartAssays)
+        then: "We expect experiments for each of the assays to be found"
+        assert experiments
 
-//    void "tests extractActivitiesFromExperiment #label"() {
-//        given: "That we have created an ETag from a list of CIDs"
-//        final Object compoundETag = restCompoundService.newETag("Compound ETags", cids);
-//        and: "That we have an Experiment Object"
-//        Experiment experiment = restExperimentService.get(experimentId)
-//
-//        and: "We call the activities method on the restExperimentService"
-//        final ServiceIterator<Value> experimentIterator = this.restExperimentService.activities(experiment, compoundETag);
-//
-//        and: "We extract the first element in the collection"
-//        Value experimentValue
-//        while (experimentIterator.hasNext()) {
-//            experimentValue = experimentIterator.next()
-//            break;
-//        }
-//        when: "We call the extractActivitiesFromExperiment method with the experimentValue"
-//        SpreadSheetActivity spreadSheetActivity = molecularSpreadSheetService.extractActivitiesFromExperiment(experimentValue)
-//        then: "We a spreadSheetActivity"
-//        assert spreadSheetActivity
-//        assert spreadSheetActivity.cid
-//        assert spreadSheetActivity.eid
-//        assert spreadSheetActivity.sid
-//        assert spreadSheetActivity.hillCurveValue
-//        where:
-//        label                                    | cids                                                      | experimentId
-//        "An existing experiment witha ctivities" | [new Long(1051569), new Long(2917647), new Long(3494575)] | new Long(519)
-//
-//    }
 
-//    void "tests findActivitiesForCompounds #label"() {
-//        given: "That we have created an ETag from a list of CIDs"
-//        final Object etag = restCompoundService.newETag("Compound ETags", cids);
-//        and: "That we have an Experiment Object"
-//        Experiment experiment = restExperimentService.get(experimentId)
-//        when: "We call the findActivitiesForCompounds() method with the experiment and the ETag"
-//        final List<SpreadSheetActivity> activities = molecularSpreadSheetService.findActivitiesForCompounds(experiment, etag)
-//        then: "We expect experiments for each of the assays to be found"
-//        assert activities
-//        where:
-//        label                                    | cids                                                      | experimentId  | expectedExperimentIds
-//        "An existing experiment witha ctivities" | [new Long(1051569), new Long(2917647), new Long(3494575)] | new Long(519) | []
-//    }
+        where:
+        label                                | cartAssays
+        "An existing assay with experiments" | [new CartAssay(assayId: new Long(519))]
+    }
+
+    void "tests extractActivitiesFromExperiment #label"() {
+        given: "That we have created an ETag from a list of CIDs"
+        final Object compoundETag = restCompoundService.newETag("Compound ETags For Activities", cids);
+        and: "That we have an Experiment Object"
+        Experiment experiment = restExperimentService.get(experimentId)
+
+        and: "We call the activities method on the restExperimentService"
+        final ServiceIterator<Value> experimentIterator = this.restExperimentService.activities(experiment, compoundETag);
+        Collection collect = experimentIterator.collect()
+        and: "We extract the first element in the collection"
+        Value experimentValue = collect.iterator().next()
+        when: "We call the extractActivitiesFromExperiment method with the experimentValue"
+        SpreadSheetActivity spreadSheetActivity = molecularSpreadSheetService.extractActivitiesFromExperiment(experimentValue)
+        then: "We a spreadSheetActivity"
+        assert spreadSheetActivity
+        assert spreadSheetActivity.cid
+        assert spreadSheetActivity.eid
+        assert spreadSheetActivity.sid
+        assert spreadSheetActivity.hillCurveValue
+        where:
+        label                                    | cids                                                   | experimentId
+        "An existing experiment with activities" | [new Long(164981), new Long(411519), new Long(483860)] | new Long(1326)
+
+    }
+
+    void "tests findActivitiesForCompounds #label"() {
+        given: "That we have created an ETag from a list of CIDs"
+        final Object etag = restCompoundService.newETag("Compound ETags", cids);
+        and: "That we have an Experiment Object"
+        Experiment experiment = restExperimentService.get(experimentId)
+        when: "We call the findActivitiesForCompounds() method with the experiment and the ETag"
+        final List<SpreadSheetActivity> activities = molecularSpreadSheetService.findActivitiesForCompounds(experiment, etag)
+        then: "We expect experiments for each of the assays to be found"
+        assert activities
+        where:
+        label                                    | cids                                                   | experimentId
+        "An existing experiment with activities" | [new Long(164981), new Long(411519), new Long(483860)] | new Long(1326)
+    }
 
     void "tests retrieveExperimentalData"() {
         when:
@@ -127,8 +125,8 @@ class MolecularSpreadSheetServiceIntegrationSpec extends IntegrationSpec {
         molSpreadSheetData.mssData.put("1_2", new MolSpreadSheetCell("3888711", MolSpreadSheetCellType.greaterThanNumeric))
         molSpreadSheetData.mssData.put("1_3", new MolSpreadSheetCell("3888711", MolSpreadSheetCellType.percentageNumeric))
         molSpreadSheetData.mssData.put("1_4", new MolSpreadSheetCell("3888711", MolSpreadSheetCellType.lessThanNumeric))
-        molSpreadSheetData.rowPointer.put(5342L,0)
-        molSpreadSheetData.rowPointer.put(5344L,0)
+        molSpreadSheetData.rowPointer.put(5342L, 0)
+        molSpreadSheetData.rowPointer.put(5344L, 0)
         molSpreadSheetData
     }
 }
