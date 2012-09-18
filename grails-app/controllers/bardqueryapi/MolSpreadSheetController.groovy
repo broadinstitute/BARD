4c95793357bea52e0a02b0ea915071af3d5b85d6
Ben Alexander <balexand@broadinstitute.org>
2012-09-18 12:11:31 -0400

    provide error message for empty mol spread sheet (36248499)

--- grails-app/controllers/bardqueryapi/MolSpreadSheetController.groovy
+++ grails-app/controllers/bardqueryapi/MolSpreadSheetController.groovy
@@ -16,8 +16,11 @@ class MolSpreadSheetController {
         if (molecularSpreadSheetService.weHaveEnoughDataToMakeASpreadsheet()) {
             MolSpreadSheetData molSpreadSheetData = molecularSpreadSheetService.retrieveExperimentalData()
             render (view:"molecularSpreadSheet", model:  [ molSpreadSheetData: molSpreadSheetData ])
-        }  else
+        }  else {
+            flash.message = "Cannot display molecular spreadsheet without at least one assay and at least one compound"
             render (view:"../bardWebInterface/index")
+        }
+
     }
 
     def sortMe(){
