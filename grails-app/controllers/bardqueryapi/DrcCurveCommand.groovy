74097238b754831ee28d4297392968491f37e408
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-20 12:49:29 -0400

    Adding code for generating dose response curves

---
+++ grails-app/controllers/bardqueryapi/DrcCurveCommand.groovy
@@ -0,0 +1,28 @@
+package bardqueryapi
+
+import org.apache.commons.collections.FactoryUtils
+import org.apache.commons.collections.ListUtils
+
+/**
+ * Command object used to parse all the search parameters coming in from the client.
+ */
+@grails.validation.Validateable
+class DrcCurveCommand {
+    Double s0
+    Double sinf
+    Double ac50
+    Double hillSlope
+    List<Double> concentrations = ListUtils.lazyList([], new ListUtilsFactory() )
+    List<Double> activities = ListUtils.lazyList([],new ListUtilsFactory() )
+
+    //TODO: Add  the needed validations here. Command object need to be valid to do request
+//    static constraints = {
+//        searchString(blank: false)
+//    }
+}
+class ListUtilsFactory implements org.apache.commons.collections.Factory {
+    public Object create() {
+        return new Double(0);
+    }
+}
+
---
+++ test/functional/bardqueryapi/DoseResponseCurveFunctionalSpec.groovy
@@ -0,0 +1,118 @@
+package bardqueryapi
+
+import grails.plugin.remotecontrol.RemoteControl
+import groovyx.net.http.HttpResponseDecorator
+import groovyx.net.http.RESTClient
+import net.sf.json.JSONObject
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.servlet.http.HttpServletResponse
+
+import static groovyx.net.http.ContentType.URLENC
+
+@Unroll
+class DoseResponseCurveFunctionalSpec extends Specification {
+    RemoteControl remote = new RemoteControl()
+    String baseUrl = remote { ctx.grailsApplication.config.grails.serverURL }
+
+    def "test Render a dose response Curve #label"() {
+        given: "That we have created a valid data set"
+         def postBody =createPostBody()
+
+        String requestUrl = "${baseUrl}/"
+        RESTClient http = new RESTClient(requestUrl)
+
+        when: 'We send an HTTP POST request to the Dose response curve service'
+        HttpResponseDecorator serverResponse = (HttpResponseDecorator) http.post(path: 'doseResponseCurve',
+                body: postBody,
+                requestContentType: URLENC)
+        then: 'We expect the a dose response curve would be generated'
+        assert serverResponse.success
+        assert serverResponse.contentType=='image/png'
+        download(serverResponse.data,"drc2")
+    }
+    private void download(final ByteArrayInputStream stream, String fileName) {
+
+        new File("testDRC_${fileName}.png").withOutputStream { out ->
+            out << stream
+        }
+    }
+    private Map createPostBody() {
+        def requestMap = [:]
+        //== Create Concentration values
+        List<Double> concentrations = [0.0000000005000000413701855f,
+                0.0000000005000000413701855f,
+                0.000000002099999951710174f,
+                0.000000002099999951710174f,
+                0.000000008000000661922968f,
+                0.000000008000000661922968f,
+                0.00000002999999892949745f,
+                0.00000002999999892949745f,
+                0.0000001350000076172364f,
+                0.0000001350000076172364f,
+                0.0000004999999987376214f,
+                0.0000004999999987376214f,
+                0.0000020999998469051206f,
+                0.0000020999998469051206f,
+                0.000007999999979801942f,
+                0.000007999999979801942f,
+                0.000035000000934815034f,
+                0.000035000000934815034f]
+        int index = 0
+        def concentrationMap = [:]
+        for(Double concentration:concentrations){
+            String key = "concentrations[" + index + "]"
+            Double value = concentration
+            concentrationMap.put(key,value)
+            ++index
+        }
+        requestMap.putAll(concentrationMap)
+        //== Create activity values
+        def activityMap = [:]
+        index = 0
+        List<Double> activities = [2.4638421535491943f,
+                -1.2127126455307007f,
+                1.3271921873092651f,
+                -1.7350740432739258f,
+                -0.6476028561592102f,
+                -1.4504003524780273f,
+                -0.31541985273361206f,
+                -6.233550071716309f,
+                -6.293687343597412f,
+                -14.393035888671875f,
+                -23.312355041503906f,
+                -36.71405029296875f,
+                -60.78630447387695f,
+                -59.16990661621094f,
+                -82.43856811523438f,
+                -85.94784545898438f,
+                -92.01828002929688f,
+                -95.36405181884766f]
+        for(Double activity:activities){
+            String key = "activities[" + index + "]"
+            Double value =activity
+            activityMap.put(key,value)
+            ++index
+        }
+        requestMap.putAll(activityMap)
+
+
+        Double ac50 = 0.00000124934001632937;
+        requestMap.put("ac50",ac50)
+
+
+
+        Double hillSlope = 0.9267182946205139;
+        requestMap.put("hillSlope",hillSlope)
+
+        Double s0 = 0.15947775542736053;
+        requestMap.put("s0",s0)
+
+        Double sinf = -98.35183715820312;
+        requestMap.put("sinf",sinf)
+
+        return requestMap
+    }
+
+}
