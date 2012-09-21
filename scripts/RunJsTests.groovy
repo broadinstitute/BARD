61c25bedd58cca0227966cab2436b305d7807239
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-20 21:09:14 -0400

    Still working on bamboo build

--- scripts/RunJsTests.groovy
+++ scripts/RunJsTests.groovy
@@ -4,11 +4,11 @@ def ant = new AntBuilder()
 ant.mkdir(dir: 'target/test-reports')
 
 //Update the codebase from GitHub
-ant.exec(executable:'xvfbdscript.sh', dir: "test/jasmine") {
+ant.exec(executable:'./xvfbdscript.sh', dir: "test/jasmine") {
     arg(value:'start')
 }
-ant.exec(executable: 'jsTestDriver.sh', dir: 'test/jasmine', failonerror: true)
+ant.exec(executable: './jsTestDriver.sh', dir: 'test/jasmine', failonerror: true)
 
-ant.exec(executable:'xvfbdscript.sh', dir: "test/jasmine") {
+ant.exec(executable:'./xvfbdscript.sh', dir: "test/jasmine") {
     arg(value:'stop')
 }
