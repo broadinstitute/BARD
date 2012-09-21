6a144feef36df3bfb263d0f6052ab08f0d6985a8
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-20 20:46:04 -0400

    Adding groovy script

--- scripts/RunJsTests.groovy
+++ scripts/RunJsTests.groovy
@@ -4,13 +4,11 @@ def ant = new AntBuilder()
 ant.mkdir(dir: 'target/test-reports')
 
 //Update the codebase from GitHub
-//ant.exec(executable:"test/jasmine/xvfbdscript.sh") {
-//    arg(value:'start')
-//}
-ant.exec(executable: '/bin/sh', dir: 'test/jasmine', failonerror: true){
-    arg(line:'-c jsTestDriver.sh')
+ant.exec(executable:'xvfbdscript.sh', dir: "test/jasmine") {
+    arg(value:'start')
 }
+ant.exec(executable: 'jsTestDriver.sh', dir: 'test/jasmine', failonerror: true)
 
-//ant.exec(executable:'xvfbdscript.sh', dir: "test/jasmine") {
-//    arg(value:'start')
-//}
+ant.exec(executable:'xvfbdscript.sh', dir: "test/jasmine") {
+    arg(value:'stop')
+}
