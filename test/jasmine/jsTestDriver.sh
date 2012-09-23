0776412979318e444ae58757dca4fc4f1b246efa
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-23 18:29:48 -0400

    Adding more debugging

--- test/jasmine/jsTestDriver.sh
+++ test/jasmine/jsTestDriver.sh
@@ -33,7 +33,12 @@ PID_XVFB="$!"      # take the process ID
 export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
-java -jar test/jasmine/lib/JsTestDriver-1.3.4.b.jar --config test/jasmine/jsTestDriver.conf --port 4224 --browser "$NEW_FIRE_FOX_PATH" --tests all --testOutput $OUTPUT_DIR
+java -jar test/jasmine/lib/JsTestDriver-1.3.4.b.jar --config test/jasmine/jsTestDriver.conf --port 4224 --browser "$NEW_FIRE_FOX_PATH" --runnerMode DEBUG --tests all --testOutput $OUTPUT_DIR
+
+RESULT=$?
 
 kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+
 echo "Done."
+
+exit $RESULT
