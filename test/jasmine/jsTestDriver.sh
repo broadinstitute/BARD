498336a47a9d491211243d385dca0e0ff61dc4c1
Jacob Asiedu <jacob.asiedu@gmail.com>
2012-09-23 12:28:42 -0400

    Adding automated testing for javascript

--- test/jasmine/jsTestDriver.sh
+++ test/jasmine/jsTestDriver.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # directory to write output XML (if this doesn't exist, the results will not be generated!)
-echo `pwd`
-
-OUTPUT_DIR="test-reports"
+OUTPUT_DIR="target/test-reports"
 mkdir $OUTPUT_DIR
 
+XVFB=`which Xvfb`
+if [ "$?" -eq 1 ];
+then
+    echo "Xvfb not found."
+    exit 1
+fi
 
 FIREFOX=`which firefox`
 if [ "$?" -eq 1 ];
@@ -18,17 +22,17 @@ fi
 #which would not work in this script
 #so we remove the '/cygdrive/c' and replace it with 'C:'
 
-
+ echo `pwd`
 NEW_FIRE_FOX_PATH=${FIREFOX/\/cygdrive\/c/C:};
-echo $FIREFOX
-echo $NEW_FIRE_FOX_PATH
+#echo $FIREFOX
+#echo $NEW_FIRE_FOX_PATH
+
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
+
 # run the tests
-java -jar test/jasmine/lib/JsTestDriver-1.3.4.b.jar --server http://127.0.0.1:9876 --config test/jasmine/jsTestDriver.conf --tests all --testOutput "$OUTPUT_DIR"
-RESULT=$?
-echo $RESULT
+java -jar test/jasmine/lib/JsTestDriver-1.3.4.b.jar --config test/jasmine/jsTestDriver.conf --port 4224 --browser "$NEW_FIRE_FOX_PATH" --tests all --testOutput $OUTPUT_DIR
 
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
-
-echo `pwd`
-
-exit $RESULT
