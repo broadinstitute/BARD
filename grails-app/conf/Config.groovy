66ea0f2e42f67a3ae074766b59c36aa92ea16726
jerilevine <jlev@broadinstitute.org>
2012-10-12 15:42:22 -0400

    Tweaking configuration so functional tests can run on a different port

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -5,7 +5,8 @@ import org.apache.log4j.DailyRollingFileAppender
 ncgc.server.root.url = "http://bard.nih.gov/api/v6"
 promiscuityscrores.root.url = "${ncgc.server.root.url}/plugins/badapple/prom/cid/"
 //override in config file for environment
-grails.serverURL = "http://localhost:8080/bardwebquery"
+server.port=8080
+grails.serverURL = "http://localhost:${server.port}/bardwebquery"
 //URL to the ROOT of the cap server
 bard.cap.home = "http://localhost:8081/BARD/"
 bard.cap.assay = "${bard.cap.home}assayDefinition/show/"
