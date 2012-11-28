0e8b17ad4c20fcb9558c1cd8bbad982d697c39a3
unknown <ycruz@PG17162.fl.ad.scripps.edu>
2012-11-28 11:38:27 -0500

    Working on OntologydataAccessService

---
+++ BARD/grails-app/services/bard/db/dictionary/OntologyDataAccessService.groovy
@@ -0,0 +1,10 @@
+package bard.db.dictionary
+
+import bard.db.dictionary.*
+
+class OntologyDataAccessService {
+
+    public List<Element> getElementsInTree(Long assayContextId, String group) {
+		
+    }
+}
