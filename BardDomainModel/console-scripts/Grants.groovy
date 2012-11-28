de361f3f4df4fdcd48d50077a5cbe477ae6c45b1
Dan Durkin <ddurkin@broadinstitute.org>
2012-11-28 14:01:27 -0500

    added xiaorong to list for grants

--- BardDomainModel/console-scripts/Grants.groovy
+++ BardDomainModel/console-scripts/Grants.groovy
@@ -3,14 +3,14 @@ import groovy.sql.Sql
 def sql = new Sql(ctx.dataSource)
 def application = grailsApplication
 
-List<String> usernames = ['SCHATWIN', 'BARD_DEV', 'SBRUDZ', 'DATA_MIG', 'YCRUZ', 'SOUTHERN', 'BARD_QA', 'BARD_CI', 'DSTONICH', 'BALEXAND', 'DDURKIN', 'JASIEDU']
+List<String> usernames = ['SCHATWIN', 'BARD_DEV', 'SBRUDZ', 'DATA_MIG', 'YCRUZ', 'SOUTHERN', 'BARD_QA', 'BARD_CI', 'DSTONICH', 'BALEXAND', 'DDURKIN', 'JASIEDU', 'XIAORONG']
 String currentUsername = application?.config?.dataSource?.username?.toUpperCase()
 
 def tablenames = sql.rows('''select table_name from user_tables order by table_name''').collect{it.TABLE_NAME}
 
-def result = sql.withBatch(100){stmt -> 
+def result = sql.withBatch(100){stmt ->
     for(tablename in tablenames){
-        try{          
+        try{
             for (username in usernames) {
                 if( username != currentUsername ){
                     String grant = "GRANT SELECT ON ${tablename} TO ${username}"
