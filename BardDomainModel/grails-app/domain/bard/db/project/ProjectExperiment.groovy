2ea31ec87965acc184dee6b877f7c3a8d139a7ae
Dan Durkin <ddurkin@broadinstitute.org>
2012-11-29 23:14:45 -0500

    Added ProjectExperiment

---
+++ BardDomainModel/grails-app/domain/bard/db/project/ProjectExperiment.groovy
@@ -0,0 +1,38 @@
+package bard.db.project
+
+import bard.db.dictionary.Element
+import bard.db.experiment.Experiment
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ddurkin
+ * Date: 11/29/12
+ * Time: 10:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ProjectExperiment {
+
+    private static final int MODIFIED_BY_MAX_SIZE = 40
+
+    Experiment experiment
+    Project project
+    Element stage
+
+    Date dateCreated
+    Date lastUpdated
+    String modifiedBy
+
+    static constraints = {
+        experiment()
+        project()
+        stage(nullable: true)
+
+
+        dateCreated(nullable: false)
+        lastUpdated(nullable: true)
+        modifiedBy(nullable: true, blank: false, maxSize: MODIFIED_BY_MAX_SIZE)
+    }
+    static mapping = {
+        id(column: "PROJECT_EXPERIMENT_ID", generator: "sequence", params: [sequence: 'PROJECT_EXPERIMENT_ID_SEQ'])
+    }
+}
