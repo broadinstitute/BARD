d59397471fd1b800e4adc7918356f72de448a73a
Dan Durkin <ddurkin@broadinstitute.org>
2012-11-29 22:28:08 -0500

    added Simons latest changes around Project_Experiment and Project_Step

--- BardDomainModel/grails-app/migrations/iteration-014/014-changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration-014/014-changelog.groovy
@@ -2,8 +2,12 @@ databaseChangeLog = {
     String bardDomainModelMigrationsDir = ctx.migrationResourceAccessor.baseDirectory
     File migrationsDir = new File(bardDomainModelMigrationsDir)
 
-    changeSet(author: 'ddurkin', id:   'iteration-014/sql/01-syncing-with-latest-model.sql', dbms: 'oracle', context: 'standard') {
+    changeSet(author: 'ddurkin', id: 'iteration-014/sql/01-syncing-with-latest-model.sql', dbms: 'oracle', context: 'standard') {
         sqlFile(path: "${migrationsDir}/iteration-014/sql/01-syncing-with-latest-model.sql", stripComments: true)
+    }
+
+    changeSet(author: 'ddurkin', id: 'iteration-014/sql/02-project-step-changes.sql', dbms: 'oracle', context: 'standard') {
+        sqlFile(path: "${migrationsDir}/iteration-014/sql/02-project-step-changes.sql", stripComments: true)
     }
 
 }
