062001ba8ef4090947b296706c0e78117a159a07
Jacob Asiedu <jasiedu@broadinstitute.org>
2012-12-15 12:38:00 -0500

    https://www.pivotaltracker.com/story/show/41276129

--- bard-web-client/web-app/js/compoundOptions.js
+++ bard-web-client/web-app/js/compoundOptions.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
 
+    $(document).on("click", "html", function () {
+        $('.popover-link').popover('hide');
+    });
     //display pop-over, need to figure why we need to do two clicks
-    $(document).on("click", ".popover-link", function(){
+    $(document).on("click", ".popover-link", function () {
         $('.popover-link').popover({
             content:function () {
                 var popId = $(this).attr('data-detail-id');
                 return $('#' + popId).html();
             },
+            trigger:'manual',
             placement:'right'
-        });
+        }).click(function (evt) {
+                evt.stopPropagation();
+                $(this).popover('show');
+            });
+
     });
-    $(document).on("click", "a.analogs", function(){
+    $(document).on("click", "a.analogs", function () {
         var searchString = $(this).attr('data-structure-search-params');
         $('#searchString').attr('value', searchString);
         $('#searchForm').submit();
