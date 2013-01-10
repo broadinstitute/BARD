7aa1055552969e9d6dd88f2e76babeb6edd94898
Dan Durkin <ddurkin@broadinstitute.org>
2013-01-10 12:14:13 -0500

    externalized the list of schema names to grant select privs
    select privs are required to use the load_data stored procedure to copy a reference set of data

--- BardDomainModel/grails-app/migrations/grant-selects.groovy
+++ BardDomainModel/grails-app/migrations/grant-selects.groovy
@@ -1,27 +1,28 @@
 databaseChangeLog = {
 
+    /**
+     * script to grant select priv for all tables in a schema to all the users
+     */
     changeSet(author: "ddurkin", id: "grant select", dbms: 'oracle', context:'grant-selects',runAlways: 'true') {
         grailsChange {
             change {
-
-                List<String> usernames = ['BALEXAND','BAMBOO','BARD_BAMBOO_CAP','BARD_BAMBOO_DATA_EXPORT',
-                                          'BARD_BAMBOO_DOMAIN_MODEL','BARD_BAMBOO_QUERY_API','BARD_CAP_CHROME',
-                                          'BARD_CAP_FIREFOX','BARD_CI','BARD_DEV','BARD_PROD','BARD_QA',
-                                          'BARD_QA1','BARD_QA_CAP','DDURKIN','DLAHR_BARD','DSTONICH',
-                                          'GWALZER_BARD','JASIEDU','JLEV','SBRUDZ','SCHATWIN','SOUTHERN',
-                                          'XIAORONG','YCRUZ']
-
-                String currentUsername = application?.config?.dataSource?.username?.toUpperCase()
+                /* so in an exerternalized config file specify
+                 * migrations.grantSelects.schemas = ['FOO','BAR']
+                 * where the schemas that should be granted select privs are enumerated
+                 */
+                def schemaNames = application?.config?.migrations?.grantSelects?.schemas
+                String currentUsername = application?.config?.dataSource?.username
 
                 def tablenames = sql.rows('''select table_name from user_tables order by table_name''').collect{it.TABLE_NAME}
 
                 def result = sql.withBatch(100){stmt ->
                     for(tablename in tablenames){
                         try{
-                            for (username in usernames) {
-                                if( username != currentUsername ){
-                                    String grant = "GRANT SELECT ON ${tablename} TO ${username}"
-                                    //println(grant)
+                            for (String schemaName in schemaNames) {
+                                // can't grant select to yourself in oracle
+                                if( schemaName.toUpperCase() != currentUsername.toUpperCase() ){
+                                    String grant = "GRANT SELECT ON ${tablename} TO ${schemaName}"
+//                                    println(grant)
                                     stmt.addBatch(grant)
                                 }
                             }
