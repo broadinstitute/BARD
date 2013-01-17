9a6bdf6ea239f087526b2a0e1c78b4b54886d745
gwalzer <gwalzer@broadinstitute.org>
2013-01-17 16:26:14 -0500

    https://www.pivotaltracker.com/story/show/42468207: Add an 'Add to Cart' option to the compound options menu

--- bard-web-client/grails-app/taglib/bardwebquery/CompoundOptionsTagLib.groovy
+++ bard-web-client/grails-app/taglib/bardwebquery/CompoundOptionsTagLib.groovy
@@ -5,7 +5,12 @@ class CompoundOptionsTagLib {
     def compoundOptions = { attrs, body ->
 
         out << render(template: "/tagLibTemplates/compoundOptions", model: [cid: attrs.cid,
-                smiles: attrs.smiles, sid: attrs.sid,
-                imageWidth:attrs.imageWidth, imageHeight:attrs.imageHeight])
+                smiles: attrs.smiles,
+                sid: attrs.sid,
+                imageWidth: attrs.imageWidth,
+                imageHeight: attrs.imageHeight,
+                name: attrs.name,
+                numActive: attrs.numActive,
+                numAssays: attrs.numAssays])
     }
 }
