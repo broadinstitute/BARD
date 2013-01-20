1cc15db852116267fcb615371b069c053f607b03
gwalzer <gwalzer@broadinstitute.org>
2013-01-20 08:22:58 -0500

    a minor tweak to the twitterBootstrapAffix script to make the implementation more correct

--- bard-web-client/web-app/js/twitterBootstrapAffix.js
+++ bard-web-client/web-app/js/twitterBootstrapAffix.js
@@ -1,8 +1,11 @@
 //This is to apply the current position to Twitter Bootstrap's Affix to make the scrolling natural
 $(document).ready(function () {
-    $('#twitterBootstrapAffixNavBar').affix({
-        offset:$('#twitterBootstrapAffixNavBar').position()
+    $('#twitterBootstrapAffixNavBar').each(function () {
+        $(this).affix({
+            offset:$(this).position()
+        });
     });
+
     //This to fix a bug in Chrome position:fixed where sometime the div disappears
     $(document).on('click', '#twitterBootstrapAffixNavBar', function () {
         window.scrollTo(window.pageXOffset, window.pageYOffset - 1);
