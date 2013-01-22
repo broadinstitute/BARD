8b9bbf0d8d7279087c66b203cfc992e86fe0e756
gwalzer <gwalzer@broadinstitute.org>
2013-01-22 15:46:23 -0500

    adding tests to Comp.groovy

--- bard-rest-api-wrapper/test/unit/bard/core/rest/spring/assays/CompUnitSpec.groovy
+++ bard-rest-api-wrapper/test/unit/bard/core/rest/spring/assays/CompUnitSpec.groovy
@@ -27,24 +27,41 @@ class CompUnitSpec extends Specification {
     }
        '''
 
+    @Shared Comp comp1 = new Comp(display: 'comp1 name', key: 'key1', value: 'value1')
+    @Shared Comp comp2 = new Comp(display: 'comp2 name', key: 'key2', value: 'value2')
+    @Shared Comp comp3 = new Comp(display: 'comp3 name', key: 'key3', value: 'value3')
+
+
     void "test serialization to Comp"() {
         when:
         final Comp comp = objectMapper.readValue(COMP, Comp.class)
         then:
-        assert comp.display==".05 um"
-        assert comp.entity=="assay"
+        assert comp.display == ".05 um"
+        assert comp.entity == "assay"
         assert !comp.entityId
-        assert comp.source== "cap-context"
-        assert comp.id==7186
-        assert comp.contextRef=="Context for percent activity"
-        assert comp.key== "screening concentration"
+        assert comp.source == "cap-context"
+        assert comp.id == 7186
+        assert comp.contextRef == "Context for percent activity"
+        assert comp.key == "screening concentration"
         assert !comp.value
         assert !comp.extValueId
         assert !comp.url
-        assert comp.displayOrder==0
-        assert comp.related=="measureRefs:22510"
+        assert comp.displayOrder == 0
+        assert comp.related == "measureRefs:22510"
     }
 
+    void "test splitForColumnLayout: #label"() {
+        when:
+        List<List<Comp>> result = Comp.splitForColumnLayout(comps)
 
+        then:
+        assert result == expectedResult
+
+        where:
+        label                 | comps                 | expectedResult
+        'one context-item'    | [comp1]               | [[comp1]]
+        'two context-items'   | [comp1, comp2]        | [[comp1], [comp2]]
+        'three context-items' | [comp1, comp2, comp3] | [[comp1, comp2], [comp3]]
+    }
 }
 
