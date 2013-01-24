2e174cbe8503dfff35910887839ef3bf744a3098
Dan Durkin <ddurkin@broadinstitute.org>
2013-01-24 15:50:53 -0500

    fix for the auto complete in the wizard - a number of fixes for the autocomplete -- if you're in an uncategorized card the autocomplete should work -- for values all children further down in the tree are searched

--- BARD/grails-app/controllers/bard/db/registration/OntologyJSonController.groovy
+++ BARD/grails-app/controllers/bard/db/registration/OntologyJSonController.groovy
@@ -6,15 +6,15 @@ import bard.db.registration.additemwizard.*;
 import grails.converters.JSON
 
 class OntologyJSonController {
-	
+
 	OntologyDataAccessService ontologyDataAccessService
 
     def index() {
 		redirect(action: "getAssayContextItems", params: params)
 	}
-	
+
 	def getAssayContextItems(){
-		if(params.assayContextId && params.assayContextId.isLong()){			
+		if(params.assayContextId && params.assayContextId.isLong()){
 			List<AssayContextItem> assayContextItems = ontologyDataAccessService.getElementsInTree(params.assayContextId.toLong(), "test")
 			println "# of AssayContextitems: " + assayContextItems.size()
 			render(contentType: "text/json") {
@@ -48,26 +48,27 @@ class OntologyJSonController {
 			List<Descriptor> descriptors = ontologyDataAccessService.getAttributeDescriptors(params.section, params.term)
 			println "# of Attribute Descriptors: " + descriptors.size()
             descriptors = descriptors.findAll{it.elementStatus != ElementStatus.Retired}
-			if(descriptors){
+            Set<Element> uniqueElements = descriptors.collect{ it.element } as Set
+			if(uniqueElements){
 				List attributes = new ArrayList();
-				for (d in descriptors) {
+				for (Element element in uniqueElements) {
 					def item = [
-						"label" : d.label,
-						"value" : d.label,
-						"elementId" : d.element.id,
+						"label" : element.label,
+						"value" : element.label,
+						"elementId" : element.id,
 					]
 					attributes.add(item)
 				}
 				render attributes as JSON
 			}
 			else{
-				render(contentType: "text/json") {				
+				render(contentType: "text/json") {
 					element "null"
 				}
 			}
 		}
 	}
-	
+
 	def getValueDescriptors(){
 		if(params?.term && params?.section && params?.attributeId){
 			println "params.term: " + params.term
@@ -75,17 +76,17 @@ class OntologyJSonController {
 			println "params.attributeId: " + params.attributeId
 			Long eid = params.attributeId.toLong()
 			List<Descriptor> descriptors = ontologyDataAccessService.getValueDescriptors(params.attributeId.toLong(), params.section, params.term)
-            descriptors = descriptors.findAll{it.elementStatus != ElementStatus.Retired}
-			if(descriptors){
-				println "# of Value Descriptors: " + descriptors.size()
+            Set<Element> uniqueElements = descriptors.collect{ it.element } as Set
+			if(uniqueElements){
+
 				List attributes = new ArrayList();
-				for (d in descriptors) {
-					def unit = d?.unit?.abbreviation
-					unit = unit ?: (d?.unit?.label ?: "")
+				for (Element element in uniqueElements) {
+					def unit = element?.unit?.abbreviation
+					unit = unit ?: (element?.unit?.label ?: "")
 					def item = [
-						"label" : d.label,
-						"value" : d.label,
-						"elementId" : d.element.id,
+						"label" : element.label,
+						"value" : element.label,
+						"elementId" : element.id,
 						"unit" : unit
 					]
 					attributes.add(item)
@@ -100,5 +101,5 @@ class OntologyJSonController {
 			}
 		}
 	}
-	
+
 }
