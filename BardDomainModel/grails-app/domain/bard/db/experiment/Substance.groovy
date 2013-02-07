a0db8ff12559593f98d7ff3319edff2e8cba8334
Phil Montgomery <pmontgom@MBRCF0-08B.(none)>
2013-02-07 17:16:29 -0500

    got rid of unused substance fields

--- BardDomainModel/grails-app/domain/bard/db/experiment/Substance.groovy
+++ BardDomainModel/grails-app/domain/bard/db/experiment/Substance.groovy
@@ -4,8 +4,6 @@ class Substance {
 
 	Integer compoundId
 	String smiles
-	BigDecimal molecularWeight
-	String substanceType
 	Date dateCreated
 	Date lastUpdated
 	String modifiedBy
@@ -19,8 +17,6 @@ class Substance {
 	static constraints = {
 		compoundId nullable: true
 		smiles nullable: true, maxSize: 4000
-		molecularWeight( nullable: true, scale: 3)
-		substanceType( blank: false, maxSize: 20, inList: ['small molecule', 'protein', 'peptide', 'antibody', 'cell', 'oligonucleotide'])
 		dateCreated(nullable: false)
 		lastUpdated nullable: true
 		modifiedBy nullable: true, maxSize: 40
