1b792c691246444f77ca681a5061a7847deaaefc
Dan Durkin <ddurkin@broadinstitute.org>
2013-02-11 16:05:19 -0500

    Work in progress for Ontology Service
    - updated Attribute and value queries to return distinct elements and added limits
    - added tests

--- BARD/grails-app/services/bard/db/dictionary/OntologyDataAccessService.groovy
+++ BARD/grails-app/services/bard/db/dictionary/OntologyDataAccessService.groovy
@@ -10,6 +10,8 @@ class OntologyDataAccessService {
     private static final String BIOLOGY_DESCRIPTOR = "biology"
 
     private static final String INSTANCE_DESCRIPTOR = "project management"
+    private static final int ATTRIBUTE_ELEMENT_DEFAULT_LIMIT = 20
+    private static final int VALUE_ELEMENT_DEFAULT_LIMIT = 20
 
     public static class ElementSummary {
         String label;
@@ -90,77 +92,101 @@ class OntologyDataAccessService {
     }
     */
 
-    public List<Descriptor> getAttributeDescriptors(String term) {
-        List<Descriptor> results = []
-        BardDescriptor.withSession { Session session ->
+
+    public List<Element> getElementsForAttributes(String term) {
+        getElementsForAttributes(term, ATTRIBUTE_ELEMENT_DEFAULT_LIMIT)
+    }
+
+    public List<Element> getElementsForAttributes(String term, Integer limit) {
+        List<Element> results = []
+        Element.withSession { Session session ->
             /**
              * selecting all the entries in Bard_tree that aren't entries in the dictionary_tree table
              * additionally, screens out the root node in the dictionary
              */
             Query query = session.createSQLQuery("""
                 select *
-                from  bard_tree bt
-                where not EXISTS( select 1 from DICTIONARY_TREE dt where dt.ELEMENT_ID= bt.element_Id and dt.PARENT_NODE_ID != 0 )
-                and lower(bt.label) like :term
-                and bt.element_status != :elementStatus
-                order by lower(bt.label)
+                from element
+                where exists (
+                                select 1
+                                from  bard_tree bt
+                                where not EXISTS( select 1 from DICTIONARY_TREE dt where dt.ELEMENT_ID= bt.element_Id and dt.PARENT_NODE_ID != 0 )
+                                and lower(bt.label) like :term
+                                and bt.element_status != :elementStatus
+                                and bt.ELEMENT_ID = element.ELEMENT_ID
+                             )
+                and rownum < :limit
+                order by lower(element.label)
             """)
-            query.addEntity(BardDescriptor)
+            query.addEntity(Element)
             query.setString('term', "%${term?.trim()?.toLowerCase()}%")
             query.setString('elementStatus', ElementStatus.Retired.name())
+            query.setInteger('limit', limit)
             query.setReadOnly(true)
             results = query.list()
         }
         return results
     }
 
-    public List<Descriptor> getValueDescriptors(Long elementId, String path, String term) {
-        List<Descriptor> results = []
-        BardDescriptor.withSession { session ->
+
+
+    public List<Element> getElementsForValues(Long elementId, String term) {
+        getElementsForValues(elementId, term, VALUE_ELEMENT_DEFAULT_LIMIT)
+    }
+
+    public List<Element> getElementsForValues(Long elementId, String term, Integer limit) {
+        List<Element> results = []
+        Element.withSession { Session session ->
             Query query = session.createSQLQuery("""
                 select *
-                from bard_tree bt
-                    join (select element_id, full_path
-                          from bard_tree
-                          where element_id = :elementId) ancestor on ancestor.full_path = substr(bt.full_path, 0, length(ancestor.full_path))
-                where lower(bt.label) like :term
-                and bt.element_status != :elementStatus
-                order by lower(bt.label)
+                from element
+                where exists ( select 1
+                               from bard_tree bt
+                               join (select element_id, full_path
+                                     from bard_tree
+                                     where element_id = :ancestorElementId) ancestor on ancestor.full_path = substr(bt.full_path, 0, length(ancestor.full_path))
+                               where lower(bt.label) like :term
+                               and bt.element_status != :elementStatus
+                               and bt.ELEMENT_ID = element.ELEMENT_ID
+                             )
+                and rownum < :limit
+                order by lower(element.label)
             """)
-            query.addEntity(BardDescriptor)
-            query.setLong("elementId", elementId)
+            query.addEntity(Element)
+            query.setLong("ancestorElementId", elementId)
             query.setString('term', "%${term?.trim()?.toLowerCase()}%")
             query.setString('elementStatus', ElementStatus.Retired.name())
+            query.setInteger('limit', VALUE_ELEMENT_DEFAULT_LIMIT)
             query.setReadOnly(true)
             results = query.list()
         }
         return results
     }
 
-	public Map<Long, String> getBaseUnits(Long elementId, Long toUnitId){
-		List<Long> resultsOne = UnitConversion.executeQuery("select uc.fromUnit.id from UnitConversion uc where toUnit.id = ?", toUnitId)
-		List<Long> resultsTwo = Element.executeQuery("select e.id from Element e where id = ?", elementId)
-		resultsOne.addAll(resultsTwo)
-		List<Long> unionAll = resultsOne
-		String parametizedString = getInParametizedQueryString(unionAll);
-		List<UnitTree> unitResults = UnitTree.executeQuery("from UnitTree ut where ut.element.id in (" + parametizedString + ")", unionAll)
-		println "# of Unit Results: " + unitResults.size()
-		Map<Long, String> unitsMap = [:];
-		for(UnitTree u in unitResults){
-			unitsMap.put(u.id, u.label)
-		}
-		return unitsMap
-	}
+    public Map<Long, String> getBaseUnits(Long elementId, Long toUnitId) {
+        List<Long> resultsOne = UnitConversion.executeQuery("select uc.fromUnit.id from UnitConversion uc where toUnit.id = ?", toUnitId)
+        List<Long> resultsTwo = Element.executeQuery("select e.id from Element e where id = ?", elementId)
+        resultsOne.addAll(resultsTwo)
+        List<Long> unionAll = resultsOne
+        String parametizedString = getInParametizedQueryString(unionAll);
+        List<UnitTree> unitResults = UnitTree.executeQuery("from UnitTree ut where ut.element.id in (" + parametizedString + ")", unionAll)
+        println "# of Unit Results: " + unitResults.size()
+        Map<Long, String> unitsMap = [:];
+        for (UnitTree u in unitResults) {
+            unitsMap.put(u.id, u.label)
+        }
+        return unitsMap
+    }
 
-	private String getInParametizedQueryString(List theList){
-		String parametizedString = "";
-		int counter = 0;
-		for(item in theList){
-			counter++
-			parametizedString = parametizedString + "?"
-			if(counter != theList.size())
-				parametizedString = parametizedString + ", "
-		}
-		return parametizedString
-	}
+    private String getInParametizedQueryString(List theList) {
+        String parametizedString = "";
+        int counter = 0;
+        for (item in theList) {
+            counter++
+            parametizedString = parametizedString + "?"
+            if (counter != theList.size())
+                parametizedString = parametizedString + ", "
+        }
+        return parametizedString
+    }
 }
