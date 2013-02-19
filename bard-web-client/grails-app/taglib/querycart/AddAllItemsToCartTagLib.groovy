65cb579ce19a8971b94151224dff327d5589e257
gwalzer <gwalzer@broadinstitute.org>
2013-02-19 15:28:23 -0500

    adding a spinning wheel to the buttons in case adding all items takes a while

--- bard-web-client/grails-app/taglib/querycart/AddAllItemsToCartTagLib.groovy
+++ bard-web-client/grails-app/taglib/querycart/AddAllItemsToCartTagLib.groovy
@@ -4,7 +4,10 @@ class AddAllItemsToCartTagLib {
 
     def selectAllItemsInPage = { attrs, body ->
 
-        out << '<input type="button" class="btn span2" id="addAllItemsToCart" value="Add All Items To Cart" name="addAllItemsToCart" mainDivName="' + attrs.mainDivName + '">'
+        out << """<div class="btn-group">
+                    <a class='btn' id='addAllItemsToCart'mainDivName='""" +
+                        attrs.mainDivName + """'><i id='addAllItemsToCartButtonIcon' class=''></i> Add All Items To Cart</a>
+                  </div>"""
 
     }
 }
--- bard-web-client/web-app/js/addAllItemsToCart.js
+++ bard-web-client/web-app/js/addAllItemsToCart.js
@@ -2,6 +2,9 @@ $(document).on('click', '#addAllItemsToCart', function () {
     var mainDivName = $(this).attr('mainDivName');
     var saveToCartElements = $('#' + mainDivName + ' input.addToCartCheckbox');
     var payloadArray = new Array();
+    var spinnerImageLink = '<img src="/bardwebclient/static/images/loading_icon.gif" alt="loading" title="loading" height="16" width="16" style="background-color: transparent;"/>';
+    var saveAllButtonElement = $('#' + mainDivName + ' a#addAllItemsToCart');
+    var buttonHtml = $(saveAllButtonElement).html()
 
     saveToCartElements.each(function (index, element) {
         var id = $(element).attr('data-cart-id');
@@ -31,14 +34,21 @@ $(document).on('click', '#addAllItemsToCart', function () {
         data:{'items':payloadArrayJSON},
         //timeout: 10000,
         beforeSend:function () {
+            $(saveAllButtonElement).find('i#addAllItemsToCartButtonIcon').removeClass(); //remove any existing icons
+            $(saveAllButtonElement).html(spinnerImageLink + buttonHtml);
         },
         success:function (data) {
             saveToCartElements.each(function (index, element) {
                 $(element).attr('checked', 'checked');
                 queryCart.publishCartChangeEvent('cart.itemAdded', element.id);
+                $(saveAllButtonElement).html(buttonHtml); //restore back the button html content
+                $(saveAllButtonElement).find('i#addAllItemsToCartButtonIcon').removeClass(); //remove any existing icons
             });
         },
-        error:function (XMLHttpRequest, textStatus, errorThrown) {},
-        complete:function () {}
+        error:function (XMLHttpRequest, textStatus, errorThrown) {
+            $(saveAllButtonElement).find('i#addAllItemsToCartButtonIcon').addClass('icon-exclamation-sign') //add an exclamation-sign to the button
+        },
+        complete:function () {
+        }
     });
 });
