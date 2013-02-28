eb5fd5eda169f24ef0b07f9a2f75a844a83fb1f0
Philip Montgomery <pmontgom@broadinstitute.org>
2013-02-27 21:37:30 -0500

    Fixed unit test for JsonResult serialization

--- BARD/test/unit/bard/db/experiment/JsonResultUnitSpec.groovy
+++ BARD/test/unit/bard/db/experiment/JsonResultUnitSpec.groovy
@@ -12,6 +12,46 @@ import spock.lang.Specification
  * To change this template use File | Settings | File Templates.
  */
 class JsonResultUnitSpec extends Specification {
+    String expected = """
+{
+  "sid" : 100,
+  "rootElem" : [ {
+    "resultId" : 100,
+    "resultTypeId" : 101,
+    "statsModifierId" : 15,
+    "resultType" : "AC50",
+    "valueNum" : 10.0,
+    "valueMin" : 0.0,
+    "valueMax" : 1000.0,
+    "replicateNumber" : 1,
+    "valueDisplay" : "valueDisplay",
+    "qualifier" : "=",
+    "related" : [ {
+      "resultId" : 100,
+      "resultTypeId" : 101,
+      "statsModifierId" : 15,
+      "resultType" : "AC50",
+      "valueNum" : 10.0,
+      "valueMin" : 0.0,
+      "valueMax" : 1000.0,
+      "replicateNumber" : 1,
+      "valueDisplay" : "valueDisplay",
+      "qualifier" : "=",
+      "relationship" : "Derives"
+    } ],
+    "contextItems" : [ {
+      "itemId" : 100,
+      "attribute" : "attribute",
+      "qualifier" : "<",
+      "valueNum" : 102.0,
+      "valueMin" : -200.0,
+      "valueMax" : 200.0,
+      "valueDisplay" : "value",
+      "valueElementId" : 100
+    } ]
+  } ]
+}
+"""
     def 'test serialization'() {
         when:
         def contextItem = new JsonResultContextItem(qualifier: "<", valueMax: 200, valueMin: -200, valueNum: 102, valueDisplay: "value", itemId: 100, attribute: "attribute", valueElementId: 100);
@@ -22,14 +62,12 @@ class JsonResultUnitSpec extends Specification {
         ObjectMapper mapper = new ObjectMapper()
         def serialized = mapper.writeValueAsString(substanceResults)
 
-        def w = mapper.writer().withDefaultPrettyPrinter();
-        println(w.writeValueAsString(substanceResults))
-//        println(w.writeValueAsString(mapper.generateJsonSchema(JsonResult.class).schemaNode));
-//        println(w.writeValueAsString(mapper.generateJsonSchema(JsonResultContextItem.class).schemaNode));
-
-        println()
+        // parse the json into simple java types (Map, List, primitives) and then do the comparison to compare insensitive
+        // to key order and spacing
+        def parsed = mapper.readValue(serialized, Object.class)
+        def expectedAsJson = mapper.readValue(serialized, Object.class)
 
         then:
-        serialized == "{\"resultId\":100,\"resultTypeId\":101,\"resultType\":\"AC50\",\"valueNum\":10.0,\"valueMin\":0.0,\"valueMax\":1000.0,\"replicateNumber\":1,\"valueDisplay\":\"valueDisplay\",\"qualifier\":\"=\"}"
+        parsed == expectedAsJson
     }
 }
