60f6033a1b521121ed0abcd65092ba0a1aabfe9e
dllahr <dlahr@broadinstitute.org>
2013-03-21 22:44:34 -0400

    add tests for 1.  contextDTO is empty and should not be loaded   2.  boundary conditions in which assay that is being loaded to has no assayContext, or has assayContext without assayContextItem

--- MinAssayAnnotParser/test/unit/bard/dm/assaycompare/minimumassayannotation/validateCreatePersist/AssayContextsValidatorCreatorAndPersistorTestSpec.groovy
+++ MinAssayAnnotParser/test/unit/bard/dm/assaycompare/minimumassayannotation/validateCreatePersist/AssayContextsValidatorCreatorAndPersistorTestSpec.groovy
@@ -29,7 +29,7 @@ class AssayContextsValidatorCreatorAndPersistorTestSpec extends Specification {
 
     private static final Map<String, String> keyValueMap = ["test key 1":"test value 1", "test key 2":"test value 2"]
 
-    void "test to not add 2 duplicate contexts"() {
+    void "test to not add 2 duplicate contextDTO's"() {
         setup:
         final Map<String, Element> findByLabelILikeMap = new HashMap<String, Element>()
         for (String key : keyValueMap.keySet()) {
@@ -43,8 +43,13 @@ class AssayContextsValidatorCreatorAndPersistorTestSpec extends Specification {
             return findByLabelILikeMap.get(label)
         }
 
+        Assay assay = Assay.build()
+        AssayContext assayContext = AssayContext.build()
+        assayContext.assayContextItems.add(AssayContextItem.build())
+        assay.assayContexts.add(assayContext)
+
         MockAssayContextsValidatorCreatorAndPersistor mockAssayContextsValidatorCreatorAndPersistor =
-            new MockAssayContextsValidatorCreatorAndPersistor()
+            new MockAssayContextsValidatorCreatorAndPersistor(assay)
 
 
         List<ContextDTO> contextDTOList = new ArrayList<ContextDTO>(2)
@@ -68,6 +73,120 @@ class AssayContextsValidatorCreatorAndPersistorTestSpec extends Specification {
         mockAssayContextsValidatorCreatorAndPersistor.assay.assayContexts.size() == 2
 
     }
+
+    void "test to not crash when encountering assay without assayContext"() {
+        setup:
+        final Map<String, Element> findByLabelILikeMap = new HashMap<String, Element>()
+        for (String key : keyValueMap.keySet()) {
+            findByLabelILikeMap.put(key, Element.build(label: key))
+
+            String value = keyValueMap.get(key)
+            findByLabelILikeMap.put(value, Element.build(label: value))
+        }
+
+        Element.metaClass.static.findByLabelIlike = { String label ->
+            return findByLabelILikeMap.get(label)
+        }
+
+        MockAssayContextsValidatorCreatorAndPersistor mockAssayContextsValidatorCreatorAndPersistor =
+            new MockAssayContextsValidatorCreatorAndPersistor(Assay.build())
+
+        List<ContextDTO> contextDTOList = new ArrayList<ContextDTO>(2)
+
+        for (int i = 0; i < 2; i++) {
+            ContextDTO contextDTO = new ContextDTO(name: "test ContextDTO", aid: 1234)
+            contextDTOList.add(contextDTO)
+
+            for (String key : keyValueMap.keySet()) {
+                String value = keyValueMap.get(key)
+
+                ContextItemDto contextItemDto = new ContextItemDto(key, value, AttributeType.Fixed)
+                contextDTO.contextItemDtoList.add(contextItemDto)
+            }
+        }
+
+        when:
+        mockAssayContextsValidatorCreatorAndPersistor.createAndPersist(contextDTOList)
+
+        then:
+        mockAssayContextsValidatorCreatorAndPersistor.assay.assayContexts.size() == 1
+    }
+
+    void "test to not crash when encountering assay whose assayContext does not have assayContextItem"() {
+        setup:
+        final Map<String, Element> findByLabelILikeMap = new HashMap<String, Element>()
+        for (String key : keyValueMap.keySet()) {
+            findByLabelILikeMap.put(key, Element.build(label: key))
+
+            String value = keyValueMap.get(key)
+            findByLabelILikeMap.put(value, Element.build(label: value))
+        }
+
+        Element.metaClass.static.findByLabelIlike = { String label ->
+            return findByLabelILikeMap.get(label)
+        }
+
+        Assay assay = Assay.build()
+        assay.assayContexts.add(AssayContext.build())
+        MockAssayContextsValidatorCreatorAndPersistor mockAssayContextsValidatorCreatorAndPersistor =
+            new MockAssayContextsValidatorCreatorAndPersistor(assay)
+
+        List<ContextDTO> contextDTOList = new ArrayList<ContextDTO>(2)
+
+        for (int i = 0; i < 2; i++) {
+            ContextDTO contextDTO = new ContextDTO(name: "test ContextDTO", aid: 1234)
+            contextDTOList.add(contextDTO)
+
+            for (String key : keyValueMap.keySet()) {
+                String value = keyValueMap.get(key)
+
+                ContextItemDto contextItemDto = new ContextItemDto(key, value, AttributeType.Fixed)
+                contextDTO.contextItemDtoList.add(contextItemDto)
+            }
+        }
+
+        when:
+        mockAssayContextsValidatorCreatorAndPersistor.createAndPersist(contextDTOList)
+
+        then:
+        mockAssayContextsValidatorCreatorAndPersistor.assay.assayContexts.size() == 2
+    }
+
+    void "test to not add when contextDTO that does not have contextItemDTO"() {
+        setup:
+        final Map<String, Element> findByLabelILikeMap = new HashMap<String, Element>()
+        for (String key : keyValueMap.keySet()) {
+            findByLabelILikeMap.put(key, Element.build(label: key))
+
+            String value = keyValueMap.get(key)
+            findByLabelILikeMap.put(value, Element.build(label: value))
+        }
+
+        Element.metaClass.static.findByLabelIlike = { String label ->
+            return findByLabelILikeMap.get(label)
+        }
+
+        MockAssayContextsValidatorCreatorAndPersistor mockAssayContextsValidatorCreatorAndPersistor =
+            new MockAssayContextsValidatorCreatorAndPersistor(Assay.build())
+
+        List<ContextDTO> contextDTOList = new ArrayList<ContextDTO>(2)
+
+        ContextDTO contextDTO = new ContextDTO(name: "test ContextDTO", aid: 1234)
+//        contextDTOList.add(contextDTO)
+//        String key = keyValueMap.keySet().iterator().next()
+//        String value = keyValueMap.get(key)
+//        ContextItemDto contextItemDto = new ContextItemDto(key, value, AttributeType.Fixed)
+//        contextDTO.contextItemDtoList.add(contextItemDto)
+
+        contextDTO = new ContextDTO(name: "test ContextDTO", aid: 1234)
+        contextDTOList.add(contextDTO)
+
+        when:
+        mockAssayContextsValidatorCreatorAndPersistor.createAndPersist(contextDTOList)
+
+        then:
+        mockAssayContextsValidatorCreatorAndPersistor.assay.assayContexts.size() == 0
+    }
 }
 
 
@@ -81,13 +200,10 @@ class MockContextLoadResultsWriter extends ContextLoadResultsWriter {
 class MockAssayContextsValidatorCreatorAndPersistor extends AssayContextsValidatorCreatorAndPersistor {
     Assay assay
 
-    MockAssayContextsValidatorCreatorAndPersistor() {
+    MockAssayContextsValidatorCreatorAndPersistor(Assay assay) {
         super("test_modified_by", new MockContextLoadResultsWriter(), false, new MockLogger())
 
-        AssayContext assayContext = AssayContext.build()
-        assayContext.assayContextItems.add(AssayContextItem.build())
-        assay = Assay.build()
-        assay.assayContexts.add(assayContext)
+        this.assay = assay
     }
 
     @Override
