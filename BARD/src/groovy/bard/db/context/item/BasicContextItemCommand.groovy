485d04e89955d97b90a3440277e1762de0255471
Dan Durkin <ddurkin@broadinstitute.org>
2013-03-27 16:51:43 -0400

    introduced BardCommand to shard some command object code

--- BARD/src/groovy/bard/db/context/item/BasicContextItemCommand.groovy
+++ BARD/src/groovy/bard/db/context/item/BasicContextItemCommand.groovy
@@ -1,11 +1,15 @@
 package bard.db.context.item
 
+import bard.db.command.BardCommand
+import bard.db.dictionary.Element
 import bard.db.experiment.ExperimentContext
 import bard.db.model.AbstractContextItem
 import bard.db.project.ProjectContext
+import bard.db.project.ProjectContextItem
 import bard.db.project.ProjectExperimentContext
 import grails.validation.Validateable
-import org.apache.commons.lang3.StringUtils
+import groovy.transform.TypeChecked
+import org.apache.commons.lang.StringUtils
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,13 +20,14 @@ import org.apache.commons.lang3.StringUtils
  * as these aren't used for most types of contextItems
  */
 @Validateable
-class BasicContextItemCommand {
+class BasicContextItemCommand extends BardCommand{
 
-    static final List<String> CONTEXT_TYPES = [ProjectContext, ExperimentContext, ProjectExperimentContext].collect{it.simpleName}
+    static final List<String> CONTEXT_TYPES = [ProjectContext, ExperimentContext, ProjectExperimentContext].collect { it.simpleName }
 
     Long contextId
-    String contextClass
+    String contextClass = "ProjectContext"
     Long attributeElementId
+    Long valueNumUnitId
 
     Long valueElementId
     String extValueId
@@ -64,4 +69,27 @@ class BasicContextItemCommand {
 //
 //        }
 //    }
+
+    AbstractContextItem createNewContextItem() {
+        AbstractContextItem contextItemToReturn = null
+        if (validate()){
+            AbstractContextItem contextItem = new ProjectContextItem()
+            contextItem.attributeElement = attemptFindById(Element, attributeElementId)
+            if (valueElementId){
+                contextItem.valueElement = attemptFindById(Element, valueElementId)
+            }
+            contextItem.extValueId = StringUtils.trimToNull(extValueId)
+            contextItem.valueDisplay = valueDisplay
+            contextItem.qualifier = StringUtils.trimToNull(qualifier)
+            contextItem.valueNum = valueNum
+            ProjectContext context = attemptFindById(ProjectContext, contextId)
+            context.addToContextItems(contextItem)
+            if (attemptSave(contextItem)){
+                contextItemToReturn = contextItem
+            }
+        }
+        contextItemToReturn
+
+    }
+
 }
