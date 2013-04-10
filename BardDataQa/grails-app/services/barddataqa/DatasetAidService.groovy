f078535f8da6420ef159467ac9565479bc953140
dllahr <dlahr@broadinstitute.org>
2013-04-10 10:59:01 -0400

    modify code to add loadOrder to dataset domain model, and then use loadOrder for query of AID's (so that AID's from previously loaded datasets are not included in later datasets)

--- BardDataQa/grails-app/services/barddataqa/DatasetAidService.groovy
+++ BardDataQa/grails-app/services/barddataqa/DatasetAidService.groovy
@@ -5,10 +5,15 @@ import org.hibernate.Query
 class DatasetAidService {
 
     private static final String datasetIdParam = "datasetId"
-        private static final String queryString = """
-select aid from bard_data_qa_dashboard.vw_project_aid_join
+    private static final String loadOrderParam = "loadOrder"
+    private static final String queryString = """
+select distinct aid from bard_data_qa_dashboard.vw_project_aid_join
   where project_uid in
     (select project_uid from bard_data_qa_dashboard.dataset_project_uid where dataset_id = :$datasetIdParam)
+  and aid not in
+    (select aid from bard_data_qa_dashboard.vw_project_aid_join where project_uid in
+      (select project_uid from bard_data_qa_dashboard.dataset_project_uid where dataset_id in
+        (select id from bard_data_qa_dashboard.dataset where load_order < :$loadOrderParam)))
   order by aid"""
 
     def sessionFactory
@@ -16,8 +21,11 @@ select aid from bard_data_qa_dashboard.vw_project_aid_join
     def convertObjectToTypeService
 
     List<Integer> lookupAidForDataset(long datasetId) {
+        Dataset dataset = Dataset.findById(datasetId)
+
         Query query = sessionFactory.getCurrentSession().createSQLQuery(queryString)
         query.setLong(datasetIdParam, datasetId)
+        query.setInteger(loadOrderParam, dataset.loadOrder)
 
         List<Integer> result = convertObjectToTypeService.convert(query.list())
 
