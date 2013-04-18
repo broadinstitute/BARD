ae93922e4ff68c6e767867ef1b90523138135dd2
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-04-18 12:16:34 -0400

    Mistakenly reverted changelog file

--- BardDomainModel/grails-app/migrations/iteration_023/023_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_023/023_changelog.groovy
@@ -9,23 +9,6 @@ databaseChangeLog = {
         sqlFile(path: "${migrationsDir}/iteration_023/01-populate-qualifier.sql", stripComments: true)
     }
 
-    changeSet(author: "pmontgom", id: "iteration-023/02-fix-conversions", dbms: 'oracle', context: 'standard') {
-        grailsChange {
-            final List<String> sqlBlocks = []
-            String text = resourceAccessor.getResourceAsStream('iteration_023/02-fix-conversions.sql').text
-            for (String sqlBlock in text.split(BACKSLASH_ONLY_OPTIONAL_WHITESPACE)) {
-                sqlBlocks.add(sqlBlock)
-            }
-
-            change {
-                for (String sqlBlock in sqlBlocks) {
-                    sql.call(sqlBlock)
-                }
-            }
-
-            checkSum(text)
-        }
-    }
     changeSet(author: "jasiedu", id: "iteration-023/03-add-parent-child-relationship-to-assay", dbms: 'oracle', context: 'standard') {
         sqlFile(path: "${migrationsDir}/iteration_023/03-add-parent-child-relationship-to-assay.sql", stripComments: true)
     }
