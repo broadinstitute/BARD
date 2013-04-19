a3a3b46eadb869e1eadf232712533c95b6320697
Philip Montgomery <pmontgom@broadinstitute.org>
2013-04-19 15:52:59 -0400

    Upgraded version of external-validation library

--- BARD/src/groovy/bard/validation/ext/BardExternalOntologyFactory.groovy
+++ BARD/src/groovy/bard/validation/ext/BardExternalOntologyFactory.groovy
@@ -1,6 +1,5 @@
 package bard.validation.ext
 
-import bard.validation.ext.DefaultExternalOntologyFactoryImpl
 import bard.validation.ext.ExternalOntologyAPI
 import bard.validation.ext.ExternalOntologyException
 import bard.validation.ext.ExternalOntologyFactory
@@ -13,31 +12,29 @@ import groovy.transform.TypeChecked
  * Time: 3:30 PM
  * To change this template use File | Settings | File Templates.
  */
-class BardExternalOntologyFactory implements ExternalOntologyFactory {
+class BardExternalOntologyFactory  {
+//    Map<String, ExternalOntologyAPI> customExternalOntologyApiMap = [:]
+    final static String NCBI_TOOL =                                                ExternalOntologyNCBI.NCBI_TOOL;
+    final static String NCBI_EMAIL =                                                ExternalOntologyNCBI.NCBI_EMAIL;
 
+    final Properties defaultProps = new Properties([(ExternalOntologyNCBI.NCBI_TOOL): 'bard', (ExternalOntologyNCBI.NCBI_EMAIL): 'default@bard.nih.gov'])
 
-    Map<String, ExternalOntologyAPI> customExternalOntologyApiMap = [:]
-
-    final Properties defaultProps = new Properties([(NCBI_TOOL): 'bard', (NCBI_EMAIL): 'default@bard.nih.gov'])
-
-    private final DefaultExternalOntologyFactoryImpl defaultExternalOntologyFactory = new DefaultExternalOntologyFactoryImpl()
-
+    private final RegisteringExternalOntologyFactory defaultExternalOntologyFactory = RegisteringExternalOntologyFactory.getInstance()
 
     public ExternalOntologyAPI getExternalOntologyAPI(String externalSite) throws ExternalOntologyException {
         return getExternalOntologyAPI(externalSite, defaultProps);
     }
 
-
-
     public ExternalOntologyAPI getExternalOntologyAPI(String externalSite, Properties props) throws ExternalOntologyException {
-        ExternalOntologyAPI externalOntologyAPI
-        if (customExternalOntologyApiMap.containsKey(externalSite)){
-            externalOntologyAPI = customExternalOntologyApiMap.get(externalSite)
-        }
-        else{
-            externalOntologyAPI = defaultExternalOntologyFactory.getExternalOntologyAPI(externalSite, props)
-        }
+//        ExternalOntologyAPI externalOntologyAPI
+//        if (customExternalOntologyApiMap.containsKey(externalSite)){
+//            externalOntologyAPI = customExternalOntologyApiMap.get(externalSite)
+//        }
+//        else{
+//            externalOntologyAPI = defaultExternalOntologyFactory.getExternalOntologyAPI(externalSite, props)
+//        }
 
+        return defaultExternalOntologyFactory.getExternalOntologyAPI(externalSite, props)
     }
 
 }
