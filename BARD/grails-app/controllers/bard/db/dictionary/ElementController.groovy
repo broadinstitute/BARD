264dad5066527f6202f08043902b417cce3bbd82
dllahr <dlahr@broadinstitute.org>
2013-04-26 16:17:25 -0400

    remove old list method, rename listPaths to list

--- BARD/grails-app/controllers/bard/db/dictionary/ElementController.groovy
+++ BARD/grails-app/controllers/bard/db/dictionary/ElementController.groovy
@@ -13,32 +13,6 @@ class ElementController {
     ModifyElementAndHierarchyService modifyElementAndHierarchyService
 
     def list() {
-
-        // Define a recursive closure to assemble the JSON
-        def buildForNode
-        buildForNode = { Element node ->
-            def map=[:]
-            map.id = node.id
-            map.text = node.label
-            if (node.elements) {
-                map.children = node.elements.collect {
-                    buildForNode(it)
-                }
-            }
-            else {
-                map.leaf = true
-            }
-            return map
-        }
-
-        Element root = Element.findByParentElementIsNull()
-
-        def builder = new JsonBuilder()
-        builder(buildForNode(root))
-        render builder.toString()
-    }
-
-    def listPaths() {
         Map parameterMap = generatePaths()
 
         if (! parameterMap.containsKey(errorMessageKey)) {
