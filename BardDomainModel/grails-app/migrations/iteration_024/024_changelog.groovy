49794a2789cf4daa693ac5ad975f54b120bdf89a
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-04-26 16:46:42 -0400

    https://www.pivotaltracker.com/story/show/48819765

--- BardDomainModel/grails-app/migrations/iteration_024/024_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_024/024_changelog.groovy
@@ -10,10 +10,8 @@ databaseChangeLog = {
     changeSet(author: "jasiedu", id: "iteration-024/01-add-parent-child-relationship-to-assay", dbms: 'oracle', context: 'standard') {
         sqlFile(path: "${migrationsDir}/iteration_024/01-add-parent-child-relationship-to-assay.sql", stripComments: true)
     }
-
-
-
-
-
+    changeSet(author: "jasiedu", id: "iteration-024/03-add-display-order-unique-constraint", dbms: 'oracle', context: 'standard') {
+        sqlFile(path: "${migrationsDir}/iteration_024/03-add-display-order-unique-constraint.sql", stripComments: true)
+    }
 }
 
