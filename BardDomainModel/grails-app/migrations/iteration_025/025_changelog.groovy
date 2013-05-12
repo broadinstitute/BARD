487749c829ed052f809ddd694d0dd1d2050ce7cd
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-05-12 17:26:24 -0400

    https://www.pivotaltracker.com/story/show/49723043

---
+++ BardDomainModel/grails-app/migrations/iteration_025/025_changelog.groovy
@@ -0,0 +1,22 @@
+package iteration_025
+
+databaseChangeLog = {
+    String bardDomainModelMigrationsDir = ctx.migrationResourceAccessor.baseDirectory
+    File migrationsDir = new File(bardDomainModelMigrationsDir)
+
+
+    changeSet(author: 'jasiedu', id: 'add trigger to recompute assay short name on assayType update', dbms: 'oracle', context: 'standard') {
+        grailsChange {
+            change {
+                sql.execute("""CREATE OR REPLACE TRIGGER TR_UPD_ASSAY_TYPE
+      AFTER UPDATE OF ASSAY_TYPE ON ASSAY
+      FOR EACH ROW BEGIN
+      MANAGE_NAMES.UPDATE_ASSAY_SHORT_NAME(:OLD.ASSAY_ID);
+END;
+""")
+            }
+        }
+    }
+
+}
+
