30d599c5c64d1c8f0c040dcc4b22baa30040f6ef
Philip Montgomery <pmontgom@broadinstitute.org>
2013-05-15 17:41:01 -0400

    Added ReadyForExtractListener to update the appropriate flag on the owning entity when data changes.

--- BARD/grails-app/conf/BootStrap.groovy
+++ BARD/grails-app/conf/BootStrap.groovy
@@ -1,14 +1,25 @@
+import bard.db.ReadyForExtractListener
 import bard.db.dictionary.OntologyDataAccessService
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.springframework.context.ApplicationContext
 import org.springframework.web.context.support.WebApplicationContextUtils
 import bard.validation.ext.ExternalOntologyPerson
 import bard.validation.ext.ExternalOntologyFactory
 
 class BootStrap {
-     OntologyDataAccessService ontologyDataAccessService
-	 ExternalOntologyFactory externalOntologyFactory
+    OntologyDataAccessService ontologyDataAccessService
+    ExternalOntologyFactory externalOntologyFactory
+    GrailsApplication grailsApplication
+
 	def init = { servletContext ->
 		externalOntologyFactory.getCreators().add(new ExternalOntologyPerson.PersonCreator())
 		ontologyDataAccessService.computeTrees(false)
+
+        def applicationContext = grailsApplication.mainContext
+        applicationContext.eventTriggeringInterceptor.datastores.each { k, datastore ->
+            println("datastore=${datastore} isinstance ${datastore instanceof org.grails.datastore.mapping.core.Datastore}")
+            applicationContext.addApplicationListener(new ReadyForExtractListener(datastore))
+        }
 	}
 	def destroy = {
 	}
