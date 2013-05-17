bd2d298c1b97a12e6c3ffb8c0d9c70a57d95c020
Dan Durkin <ddurkin@broadinstitute.org>
2013-05-17 12:43:13 -0400

    Change scripts for https://www.pivotaltracker.com/s/projects/670441/stories/49291799
    - this adds 2 additional columns to element and introduces a production-data-update context targeted at changing some data values in production.
    - moved the manage-audit-procedures to the top of the stack of potential final includes in the root changelog
    -- this that some config changes in the auditing could be applied prior to running the ontology stored procedures, previously we were auditing the _Tree tables which would require a context user being set to rebuild them, we're no longer auditing these tables

--- BardDomainModel/grails-app/migrations/iteration_025/025_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_025/025_changelog.groovy
@@ -44,67 +44,11 @@ END;
     }
 
 
-    changeSet(author: "ddurkin", id: "iteration_025/01-element-data-update.sql", dbms: "oracle", context: "production-data-update") {
-        grailsChange {
-            change {
-                sql.execute("""BEGIN
-                               bard_context.set_username('schatwin');
-                               manage_ontology.swap_element_id(1865, 1853);
-                               manage_ontology.swap_element_id(1954, 1527);
-                               manage_ontology.swap_element_id(1955, 735);
-                               manage_ontology.swap_element_id(1956, 1396);
-                               manage_ontology.swap_element_id(1957, 1632);
-                               manage_ontology.swap_element_id(1958, 1853);
-                               manage_ontology.swap_element_id(1959, 1446);
-                               manage_ontology.swap_element_id(1961, 790);
-                               manage_ontology.swap_element_id(1963, 1853);
-                               manage_ontology.swap_element_id(1964, 1842);
-                               manage_ontology.swap_element_id(1965, 1459);
-                               manage_ontology.swap_element_id(1966, 1890);
-                               manage_ontology.swap_element_id(1967, 1866);
-                               manage_ontology.swap_element_id(1968, 726);
-                               manage_ontology.swap_element_id(1969, 1938);
-                               manage_ontology.swap_element_id(2008, 1850);
-                               manage_ontology.swap_element_id(2015, 1896);
-                               manage_ontology.swap_element_id(2019, 415);
-                               manage_ontology.swap_element_id(2021, 1389);
-                               END;
-                               """)
-                sql.execute("delete from element where element_id = 1865")
-                sql.execute("delete from element where element_id = 1954")
-                sql.execute("delete from element where element_id = 1955")
-                sql.execute("delete from element where element_id = 1956")
-                sql.execute("delete from element where element_id = 1957")
-                sql.execute("delete from element where element_id = 1958")
-                sql.execute("delete from element where element_id = 1959")
-                sql.execute("delete from element where element_id = 1961")
-                sql.execute("delete from element where element_id = 1963")
-                sql.execute("delete from element where element_id = 1964")
-                sql.execute("delete from element where element_id = 1965")
-                sql.execute("delete from element where element_id = 1966")
-                sql.execute("delete from element where element_id = 1967")
-                sql.execute("delete from element where element_id = 1968")
-                sql.execute("delete from element where element_id = 1969")
-                sql.execute("delete from element where element_id = 2008")
-                sql.execute("delete from element where element_id = 2015")
-                sql.execute("delete from element where element_id = 2019")
-                sql.execute("delete from element where element_id = 2021")
-
-            }
-        }
-        sqlFile(path: "${migrationsDir}/iteration_025/01-element-data-update.sql", stripComments: true)
+    changeSet(author: "ddurkin", id: "iteration_025/01-add-element-columns.sql", dbms: "oracle", context: "standard") {
+        sqlFile(path: "${migrationsDir}/iteration_025/01-add-element-columns.sql", stripComments: true)
     }
 
-    changeSet(author: "ddurkin", id: "iteration_025/02-add-element-columns.sql", dbms: "oracle", context: "standard") {
-        sqlFile(path: "${migrationsDir}/iteration_025/02-add-element-columns.sql", stripComments: true)
-        grailsChange {
-            change {
-                sql.execute("DROP SEQUENCE bard_ontology_seq")
-            }
-        }
-    }
-
-    changeSet(author: "ddurkin", id: "iteration_025/03-update-element-data.sql", dbms: "oracle", context: "production-data-update") {
+    changeSet(author: "ddurkin", id: "iteration_025/02-element-update-data-for-2-new-columns.sql", dbms: "oracle", context: "production-data-update") {
         grailsChange {
             change {
                 sql.execute("""BEGIN
@@ -113,7 +57,7 @@ END;
                                """)
             }
         }
-        sqlFile(path: "${migrationsDir}/iteration_025/03-update-element-data.sql", stripComments: false) // there was a --- in a string that was getting stripped and messing up the executed sql
+        sqlFile(path: "${migrationsDir}/iteration_025/02-element-update-data-for-2-new-columns.sql", stripComments: false) // there was a --- in a string that was getting stripped and messing up the executed sql
     }
 
 }
