6fdaf06a6903484998c0c07f61774ca2ea449740
Steve Brudz <sbrudz@broadinstitute.org>
2013-05-20 14:35:01 -0400

    adding script to create data QA summary views

---
+++ Data Scripts/data_qa_dashboard_summary.sql
@@ -0,0 +1,7 @@
+--
+-- This script creates a set of views for use by a Spotfire visualization that
+-- will show the overall progress of the data QA process for each MLPCN center.
+-- The intended audience are the center PIs, the NIH, and the people performing
+-- the data QA.
+--
+
