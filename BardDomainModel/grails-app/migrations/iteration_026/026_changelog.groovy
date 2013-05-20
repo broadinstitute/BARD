da6c4685f6b1349327a473ce380eb19cc05cc51a
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-05-20 13:50:25 -0400

    https://www.pivotaltracker.com/story/show/48933305

--- BardDomainModel/grails-app/migrations/iteration_026/026_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_026/026_changelog.groovy
@@ -24,5 +24,24 @@ databaseChangeLog = {
             }
         }
     }
+    changeSet(author: "jasiedu", id: "iteration-026/02-add-curators-and-Role", dbms: "oracle", context: "standard") {
+        grailsChange {
+            change {
+                sql.execute("""BEGIN
+                               bard_context.set_username('jasiedu');
+                               END;
+                               """)
+            }
+        }
+        sqlFile(path: "${migrationsDir}/iteration_026/02-add-curators-and-Role.sql", stripComments: true)
+        grailsChange {
+            change {
+                sql.execute("""BEGIN
+                               bard_context.clear_username();
+                               END;
+                               """)
+            }
+        }
+    }
 }
 
