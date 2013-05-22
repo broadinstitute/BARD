4aa1a440cd96a1246b7d8803a94f04a13bc6acc3
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-05-22 17:21:34 -0400

    Fixing issues with cloning assays. Measure tree should now show up even when cloning fails

--- BARD/grails-app/controllers/bard/db/registration/AssayDefinitionController.groovy
+++ BARD/grails-app/controllers/bard/db/registration/AssayDefinitionController.groovy
@@ -30,17 +30,15 @@ class AssayDefinitionController {
 
     def cloneAssay(Long id) {
         Assay assay = Assay.get(id)
-        JSON measureTreeAsJson = null
         try {
             assay = assayService.cloneAssayForEditing(assay, springSecurityService.principal?.username)
             assay = assayService.recomputeAssayShortName(assay)
-            measureTreeAsJson = new JSON(measureTreeService.createMeasureTree(assay, false))
         } catch (ValidationException ee) {
             assay = Assay.get(id)
             flash.message = "Cannot clone assay definition with id \"${id}\" probably because of data migration issues. Please email the BARD team at bard-users@broadinstitute.org to fix this assay"
         }
+        JSON measureTreeAsJson = new JSON(measureTreeService.createMeasureTree(assay, false))
         render(view: "show", model: [assayInstance: assay, measureTreeAsJson: measureTreeAsJson])
-        // redirect(action: "show", id: assay.id)
     }
 
     def save() {
