ad61da62fcd45517cd35eafd6c1087860e827dbf
Philip Montgomery <pmontgom@broadinstitute.org>
2013-06-07 15:23:13 -0400

    Removed a broken service and an unneeded acl migration

--- BardDomainModel/grails-app/migrations/iteration_027/027_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_027/027_changelog.groovy
@@ -8,18 +8,5 @@ databaseChangeLog = {
     changeSet(author: "jasiedu", id: "iteration-027/01-create-acl-tables", dbms: "oracle", context: "standard") {
         sqlFile(path: "${migrationsDir}/iteration_027/01-create-acl-tables.sql", stripComments: true)
     }
-    changeSet(author: 'jasiedu', id: 'populate role table with team names', dbms: 'oracle', context: 'standard') {
-        grailsChange {
-            change {
-                String query = """SELECT TEAM_NAME FROM TEAM"""
-                sql.eachRow(query){ row ->
-                    String teamName = row.TEAM_NAME
-                    teamName = teamName.replaceAll("\\s+","_").toUpperCase()
-                    String insertStatement = "INSERT INTO ROLE (ROLE_ID, AUTHORITY,DISPLAY_NAME,VERSION,MODIFIED_BY,DATE_CREATED) VALUES (ROLE_ID_SEQ.NEXTVAL, ${teamName},${row.TEAM_NAME},0,'jasiedu',SYSDATE)"
-                    sql.execute(insertStatement)
-                }
-            }
-        }
-    }
 }
 
