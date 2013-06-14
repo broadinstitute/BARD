ff5d57a618ad6607c219d9201f85f1eead0a1ef3
Dan Durkin <ddurkin@broadinstitute.org>
2013-06-14 15:46:25 -0400

    added queries and tests for utilizing expectedValueType

--- BARD/test/integration/bard/db/dictionary/OntologyDataAccessServiceIntegrationSpec.groovy
+++ BARD/test/integration/bard/db/dictionary/OntologyDataAccessServiceIntegrationSpec.groovy
@@ -32,14 +32,14 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
     void setup() {
         BardContextUtils.setBardContextUsername(sessionFactory.currentSession, 'integrationTestUser')
         SpringSecurityUtils.reauthenticate('integrationTestUser', null)
-        grandParent = BardDescriptor.build(fullPath: "grandParent", leaf: false, label: 'grandParent')
+        grandParent = BardDescriptor.build(fullPath: "BARD> grandParent", leaf: false, label: 'grandParent')
         String parentFullPath = "${grandParent.fullPath}> parent"
         parent = BardDescriptor.build(fullPath: parentFullPath, leaf: false, parent: grandParent, label: 'parent')
         dictionaryParent = BardDictionaryDescriptor.build()
         parent.save(flush: true)
     }
 
-    void "test getValueDescriptors with direct Children desc: '#desc' expectedLabels: #expectedLabels"() {
+    void "test getElementsForValues with direct Children desc: '#desc' expectedLabels: #expectedLabels"() {
 
         given:
         createDescendants(parent, childProps)
@@ -67,7 +67,7 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
         "3 children sorted by label case insensitive"  | ['a_child', 'B_child', 'c_child'] | 'child'    | [[elementStatus: Published, label: 'c_child'], [elementStatus: Published, label: 'B_child'], [elementStatus: Published, label: 'a_child']]
     }
 
-    void "test getValueDescriptors with grandChildren desc: '#desc' expectedLabels: #expectedLabels"() {
+    void "test getElementsForValues with grandChildren desc: '#desc' expectedLabels: #expectedLabels"() {
 
         given:
         createDescendants(parent, childProps)
@@ -95,7 +95,7 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
         "3 children sorted by label case insensitive"  | ['a_child', 'B_child', 'c_child'] | 'child'    | [[elementStatus: Published, label: 'c_child'], [elementStatus: Published, label: 'B_child'], [elementStatus: Published, label: 'a_child']]
     }
 
-    void "test getValueDescriptors wildcard and escaping desc: '#desc' expectedLabels: #expectedLabels"() {
+    void "test getElementsForValues wildcard and escaping desc: '#desc' expectedLabels: #expectedLabels"() {
 
         given:
         createDescendants(parent, childProps)
@@ -121,7 +121,7 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
         "1 hit \\ escaped properly in abbreviation"     | ['child1']     | '\\ch1'    | [[elementStatus: Published, label: 'child1', abbreviation: '\\ch1']]
     }
 
-    void "test getAttributeDescriptors desc: '#desc' expectedLabels: #expectedLabels"() {
+    void "test getElementsForAttributes desc: '#desc' expectedLabels: #expectedLabels"() {
 
         given:
         List<BardDescriptor> bardDescriptors = createDescendants(parent, childProps)
@@ -161,7 +161,7 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
         "2 children sorted by label case insensitive"  | ['a_child', 'B_child', 'c_child'] | 'child'    | []                    | [[elementStatus: Published, label: 'c_child', expectedValueType: NUMERIC], [elementStatus: Published, label: 'B_child', expectedValueType: NUMERIC], [elementStatus: Published, label: 'a_child', expectedValueType: NUMERIC]]
     }
 
-    void "test getAttributeDescriptors wildcard and escaping desc: '#desc' expectedLabels: #expectedLabels"() {
+    void "test getElementsForAttributes wildcard and escaping desc: '#desc' expectedLabels: #expectedLabels"() {
 
         given:
         List<BardDescriptor> bardDescriptors = createDescendants(parent, childProps)
@@ -187,6 +187,87 @@ class OntologyDataAccessServiceIntegrationSpec extends IntegrationSpec {
         "1 hit \\ escaped properly in label"            | ['\\child1']   | '\\child1' | [[elementStatus: Published, label: '\\child1', abbreviation: 'ch1', expectedValueType: NUMERIC]]
         "1 hit \\ escaped properly in abbreviation"     | ['child1']     | '\\ch1'    | [[elementStatus: Published, label: 'child1', abbreviation: '\\ch1', expectedValueType: NUMERIC]]
     }
+
+
+    void "test getDescriptorsForAttributes desc: '#desc'"() {
+
+        given:
+
+        BardDescriptor parentA = BardDescriptor.build(fullPath: "${grandParent.fullPath}> parent A", leaf: false, parent: grandParent, label: "parent A")
+        createDescendants(parentA, parentAChildren).each { println(it.fullPath) }
+
+        BardDescriptor parentB = BardDescriptor.build(fullPath: "${grandParent.fullPath}> parent B", leaf: false, parent: grandParent, label: "parent B")
+        createDescendants(parentB, parentBChildren).each { println(it.fullPath) }
+
+        when:
+        List<BardDescriptor> results = ontologyDataAccessService.getDescriptorsForAttribute(startOfFullPath)
+
+
+        then:
+        expectedLabels == results*.label
+
+        where:
+        desc                                       | expectedLabels       | startOfFullPath          | parentAChildren                                                               | parentBChildren
+        "0 children"                               | []                   | null                     | []                                                                            | []
+        "0 children due to retired"                | []                   | null                     | [[elementStatus: Retired, label: 'childA', expectedValueType: NUMERIC]]       | []
+        "0 children due to expectedValueType none" | []                   | null                     | [[elementStatus: Published, label: 'childA', expectedValueType: NONE]]        | []
+        "1 child with status Published"            | ['childA']           | null                     | [[elementStatus: Published, label: 'childA', expectedValueType: NUMERIC]]     | []
+        "1 child with status Pending"              | ['childA']           | null                     | [[elementStatus: Pending, label: 'childA', expectedValueType: NUMERIC]]       | []
+        "1 child with status Deprecated"           | ['childA']           | null                     | [[elementStatus: ES.Deprecated, label: 'childA', expectedValueType: NUMERIC]] | []
+
+        "0 children due to startOfFullPath"        | []                   | 'notFound'               | [[elementStatus: Published, label: 'childA', expectedValueType: NUMERIC]]     | [[elementStatus: Published, label: 'childB', expectedValueType: NUMERIC]]
+        "1 children due to startOfFullPath"        | ['childA']           | 'grandParent> parent A>' | [[elementStatus: Published, label: 'childA', expectedValueType: NUMERIC]]     | []
+        "0 children due to startOfFullPath"        | []                   | 'grandParent> parent B>' | [[elementStatus: Published, label: 'childA', expectedValueType: NUMERIC]]     | []
+        "2 children"                               | ['childA', 'childB'] | null                     | [[elementStatus: Published, label: 'childA', expectedValueType: NUMERIC]]     | [[elementStatus: Published, label: 'childB', expectedValueType: NUMERIC]]
+        "1 child of 2 due to Retire"               | ['childB']           | null                     | [[elementStatus: Retired, label: 'childA', expectedValueType: NUMERIC]]       | [[elementStatus: Published, label: 'childB', expectedValueType: NUMERIC]]
+
+    }
+
+    void "test getDescriptorsForValues with direct Children desc: '#desc' expectedLabels: #expectedLabels"() {
+
+        given:
+        createDescendants(parent, childProps)
+
+        when:
+        List<BardDescriptor> results = ontologyDataAccessService.getDescriptorsForValues(parent.element.id)
+
+        then:
+
+        expectedLabels == results*.label
+
+        where:
+        desc                                          | expectedLabels                    | childProps
+        "0 children"                                  | []                                | []
+        "0 children due to Retired"                   | []                                | [[elementStatus: Retired, label: 'child1']]
+        "1 child with status Published"               | ['child1']                        | [[elementStatus: Published, label: 'child1']]
+        "1 child with status Pending"                 | ['child1']                        | [[elementStatus: Pending, label: 'child1']]
+        "1 child with status Deprecated"              | ['child1']                        | [[elementStatus: ES.Deprecated, label: 'child1']]
+        "1 child of 2 due to Retired"                 | ['child2']                        | [[elementStatus: Retired, label: 'child1'], [elementStatus: Published, label: 'child2']]
+        "3 children sorted by label case insensitive" | ['a_child', 'B_child', 'c_child'] | [[elementStatus: Published, label: 'c_child'], [elementStatus: Published, label: 'B_child'], [elementStatus: Published, label: 'a_child']]
+    }
+
+    void "test getDescriptorsForValues with grandChildren desc: '#desc' expectedLabels: #expectedLabels"() {
+
+        given:
+        createDescendants(parent, childProps)
+
+        when:
+        List<BardDescriptor> results = ontologyDataAccessService.getDescriptorsForValues(parent.element.id)
+
+        then:
+        expectedLabels == results*.label
+
+        where:
+        desc                                          | expectedLabels                    | childProps
+        "0 children"                                  | []                                | []
+        "0 children due to Retired"                   | []                                | [[elementStatus: Retired, label: 'child1']]
+        "1 child with status Published"               | ['child1']                        | [[elementStatus: Published, label: 'child1']]
+        "1 child with status Pending"                 | ['child1']                        | [[elementStatus: Pending, label: 'child1']]
+        "1 child with status Deprecated"              | ['child1']                        | [[elementStatus: ES.Deprecated, label: 'child1']]
+        "1 child of 2 due to Retired"                 | ['child2']                        | [[elementStatus: Retired, label: 'child1'], [elementStatus: Published, label: 'child2']]
+        "3 children sorted by label case insensitive" | ['a_child', 'B_child', 'c_child'] | [[elementStatus: Published, label: 'c_child'], [elementStatus: Published, label: 'B_child'], [elementStatus: Published, label: 'a_child']]
+    }
+
 
     void "test getConvertibleUnits #desc"() {
 
