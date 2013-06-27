b65dc37f537605ee1046689c5e44949c49186f4b
Yasel Cruz <ycruz@scripps.edu>
2013-06-27 14:22:27 -0400

    Completed story PT50398931 - Script to find and log context items that do not pass orm validation

---
+++ BARD/userScripts/validateExperimentContextItemsOrm.groovy
@@ -0,0 +1,53 @@
+import bard.db.experiment.ExperimentContextItem
+import bard.db.registration.AssayContextItem
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ycruz
+ * Date: 6/26/13
+ * Time: 4:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+SpringSecurityUtils.reauthenticate('integrationTestUser', null)
+
+println("Initializing output file...")
+String dirLocation = "C:\\BardData"
+File dir = new File(dirLocation)
+if(!dir.exists()){
+    if(dir.mkdir())
+        println("Output directory created")
+    else
+        println("Failed to create directory for output file")
+}
+String outputFile =  dirLocation + "/experiment_context_items_validation.txt"
+FileWriter fileWriter = new FileWriter(outputFile, false)
+fileWriter.write("List of ExperimentContextItems that don't pass validation: \n\n")
+fileWriter.flush()
+writeToFile = {message ->
+    fileWriter.write(message + "\n")
+    fileWriter.flush()
+}
+
+println("Retrieving ExperimentContextItems...")
+
+List<ExperimentContextItem> experimentContextItems = ExperimentContextItem.list()
+println("Ready to process ${experimentContextItems.size()} of Experiment Context Items in BARD")
+writeToFile("# of Experiment Context Items in BARD: ${experimentContextItems.size()}")
+
+int itemsWithErrors = 0;
+println("Detecting ExperimentContextItems with errors...")
+experimentContextItems.each { ExperimentContextItem item ->
+    if (!item.validate()) {
+        writeToFile("Experiment [ID: ${item.experimentContext.experimentId}] ExperimentContextItem [ID: ${item.id}]-> # of Errors: [${item.errors.errorCount}]")
+        itemsWithErrors++
+        item.errors.allErrors.each{
+            writeToFile(it.toString())
+        }
+    }
+}
+writeToFile("# of ExperimentContextItems with Errors: ${itemsWithErrors}")
+println("Processing has finished. Output file: " + outputFile)
+println("# of ExperimentContextItems with Errors: ${itemsWithErrors}")
+fileWriter.close()
