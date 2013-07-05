123984da74789af03b51b1f4b7213252c5d60286
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-07-05 06:39:16 -0400

    Tweaking canEdit method to get around this bug http://jira.grails.org/browse/GPSPRINGSECURITYACL-23

--- BARD/grails-app/controllers/bard/db/registration/AssayDefinitionController.groovy
+++ BARD/grails-app/controllers/bard/db/registration/AssayDefinitionController.groovy
@@ -445,13 +445,15 @@ class EditingHelper {
 
     boolean canEdit(PermissionEvaluator permissionEvaluator, SpringSecurityService springSecurityService, domainInstance) {
         final boolean isAdmin = SpringSecurityUtils?.ifAnyGranted('ROLE_BARD_ADMINISTRATOR')
+        if(isAdmin){
+            return true
+        }
 
         def auth = springSecurityService?.authentication
 
-        final boolean hasPermission = permissionEvaluator?.hasPermission(auth, domainInstance, BasePermission.ADMINISTRATION)
+        Class<?> clazz = org.springframework.util.ClassUtils.getUserClass(domainInstance.getClass());
 
-        return isAdmin || hasPermission
-
+        return permissionEvaluator?.hasPermission(auth, domainInstance.id,clazz.name,BasePermission.ADMINISTRATION)
     }
 
     def generateAndRenderJSONResponse(Long currentVersion, String modifiedBy, String shortName, Date lastUpdated, final String newValue) {
