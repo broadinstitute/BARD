d38df1472feabd95e7e17adf8973ac371af9c68f
Philip Montgomery <pmontgom@broadinstitute.org>
2013-08-19 17:27:17 -0400

    Changed filename of generated zip to package.zip

--- BARD/scripts/CreateZip.groovy
+++ BARD/scripts/CreateZip.groovy
@@ -4,28 +4,33 @@ import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.IOUtils
 
 includeTargets << grailsScript("_GrailsInit")
-includeTargets << grailsScript("Compile")
 
 target(main: "Create zip file of all files in this tree") {
-  def targetFile = new File("./target/archive.zip")
-  def dir = new File(".")
+  def targetFile = new File("./target/package.zip")
   FileOutputStream fos = new FileOutputStream(targetFile);
   ZipOutputStream zos = new ZipOutputStream(fos);
   println "Writing zip file to ${targetFile.absolutePath}"
-  dir.eachFileRecurse (FileType.FILES) { file ->
-//    println(file)
-    if(targetFile.absolutePath == file.absolutePath) {
-      return
+  for(dir in [new File("."), new File("../BardDomainModel")]) {
+    dir.eachFileRecurse (FileType.FILES) { file ->
+      if(targetFile.absolutePath == file.absolutePath) {
+        return
+      }
+
+      def fn = file.toString()
+      if(fn.startsWith("./")) {
+        fn = "BARD/"+fn.substring(2)
+      } else if(fn.startsWith("../")) {
+        fn = fn.substring(3)
+      }
+      ZipEntry ze = new ZipEntry(fn);
+      zos.putNextEntry(ze);
+      FileInputStream input = new FileInputStream(file)
+
+      IOUtils.copy(input, zos);
+      
+      input.close()
+      zos.closeEntry()
     }
-
-    ZipEntry ze = new ZipEntry(file.toString());
-    zos.putNextEntry(ze);
-    FileInputStream input = new FileInputStream(file)
-
-    IOUtils.copy(input, zos);
-    
-    input.close()
-    zos.closeEntry()
   }
   zos.close()
 }
