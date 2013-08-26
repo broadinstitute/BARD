1392b274b2a32cc048663eb93b2b08b1d085ce90
Philip Montgomery <pmontgom@broadinstitute.org>
2013-08-26 11:59:25 -0400

    merging in fix to migration which was in a checkin on a different branch

--- BardDomainModel/grails-app/migrations/iteration_032/01-remap-context-group.sql
+++ BardDomainModel/grails-app/migrations/iteration_032/01-remap-context-group.sql
@@ -10,9 +10,9 @@ update ASSAY_CONTEXT set context_type = 'Assay Design' where context_group in ('
 update ASSAY_CONTEXT set context_type = 'Assay Readout' where context_group in ('assay protocol> assay readout>', 'Assay Readout');
 update ASSAY_CONTEXT set context_type = 'Assay Components' where context_group in ('assay protocol> assay component>', 'Assay Components');
 update ASSAY_CONTEXT set context_type = 'Experimental Variables' where context_group in ('project management> project information>', 'project management> experiment>', 'Experimental Variables', 'result type> item count>');
-update ASSAY_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>') or context_group is null;
-update EXPRMT_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>') or context_group is null;
-update PRJCT_EXPRMT_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>') or context_group is null;
+update ASSAY_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>','Unclassified') or context_group is null;
+update EXPRMT_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>','Unclassified') or context_group is null;
+update PRJCT_EXPRMT_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>','Unclassified') or context_group is null;
 update PROJECT_CONTEXT set context_type = 'Unclassified' where context_group in ('unclassified>','Unclassified') or context_group is null;
 
 alter table ASSAY_CONTEXT add constraint CK_ASSAY_CTX_TYPE CHECK ( CONTEXT_TYPE is not null and CONTEXT_TYPE in ('Biology','Assay Protocol','Assay Design','Assay Readout','Assay Components','Experimental Variables','Unclassified') );
