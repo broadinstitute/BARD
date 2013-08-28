37df4bce008c82edd89cef4003284a36d51acad2
Dan Durkin <ddurkin@broadinstitute.org>
2013-08-28 16:12:11 -0400

    created a helper script to copy to git mv files over from bard-web-client to BARD (CAP)
    this just did the src dir

---
+++ BARD/console-scripts/GitMvHelper.groovy
@@ -0,0 +1,32 @@
+import groovy.io.FileType
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ddurkin
+ * Date: 8/15/13
+ * Time: 4:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+String srcRoot = 'bard-web-client'
+String dstRoot = 'BARD'
+def dirs = ['src']
+for(String relativeDir in dirs){
+    final String srcDir = "$srcRoot/$relativeDir"
+    executeCommand("cp -r ${srcDir} ${dstRoot}")
+    new File(srcDir).traverse([type: FileType.FILES, ]) { File file ->
+        final String srcPath = file.getPath()
+        executeCommand("git mv -f ${srcPath} ${srcPath.replace(srcDir, "$dstRoot/$relativeDir")}")
+    }
+}
+
+private executeCommand(String cmd){
+    println(cmd)
+    def proc = cmd.execute()
+    proc.waitFor()
+    final exitCode = proc.exitValue()
+    if(exitCode!=0) {
+        println "return code: ${ exitCode}"
+        println "stderr: ${proc.err.text}"
+        println "stdout: ${proc.in.text}"
+    }
+}
