1bc98f54e782d10474480edad1a4bbbeab9745d3
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-08-29 09:26:53 -0400

    Fixing a bunch of issues around experiment documents: 1) Edit Experiment Protocol results in "Another user has updated this page while you were editing. Please reload the page, review the current values, and update them if needed."
    2) Edit Experiment Description results in "Another user has updated this page while you were editing. Please reload the page, review the current values, and update them if needed." message\
    
    3) Edit Experiment External URL results in "Another user has updated this page while you were editing. Please reload the page, review the current values, and update them if needed."

---
+++ BardDomainModel/grails-app/migrations/iteration_032/02-add-constraint-expt-doc.sql
@@ -0,0 +1,8 @@
+-- Try to make last updated field not null to match GORM
+update EXPERIMENT_DOCUMENT set LAST_UPDATED = DATE_CREATED WHERE LAST_UPDATED IS NULL;
+-- Add not null constraint to LAST Updated column
+alter table EXPERIMENT_DOCUMENT modify (LAST_UPDATED not null);
+-- Drop the constraint on the document type
+alter table EXPERIMENT_DOCUMENT drop constraint CK_EXPERIMENT_DOCUMENT_TYPE;
+-- Re-apply constraint with ; replace Paper with Publication
+alter table EXPERIMENT_DOCUMENT add constraint CK_EXPERIMENT_DOCUMENT_TYPE CHECK (Document_Type In ('Description', 'Protocol', 'Comments', 'Publication', 'External URL', 'Other') );
