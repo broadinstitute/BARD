009aeb52c84d083f6ae16d0ac0dcb34d7e875b6b
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-09-10 21:24:06 -0400

    Fixing data export integration test

--- dataExport/test/integration/dataexport/registration/AssayExportHelperServiceIntegrationSpec.groovy
+++ dataExport/test/integration/dataexport/registration/AssayExportHelperServiceIntegrationSpec.groovy
@@ -37,6 +37,7 @@ class AssayExportHelperServiceIntegrationSpec extends IntegrationSpec {
                 'ASSAY_CONTEXT_MEASURE_ID_SEQ',
                 'ASSAY_DOCUMENT_ID_SEQ',
                 'PANEL_ID_SEQ',
+                'PANEL_ASSAY_ID_SEQ',
                 'ELEMENT_ID_SEQ',
                 'MEASURE_ID_SEQ'].each {
             this.resetSequenceUtil.resetSequence(it)
