8cb583bf111fcf4b4e684bcdb0f927902d3ded06
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-09-28 19:07:38 -0400

    Fixing broken functional tests

--- BARD/test/functional/bard/db/registration/MergeAssayDefintionControllerFunctionalSpec.groovy
+++ BARD/test/functional/bard/db/registration/MergeAssayDefintionControllerFunctionalSpec.groovy
@@ -27,61 +27,20 @@ class MergeAssayDefintionControllerFunctionalSpec extends BardControllerFunction
 
     @Shared
     List<Long> assayIdList = []  //we keep ids of all assays here so we can delete after all the tests have finished
-    @Shared
-    Map experimentData = [:]
 
     def setupSpec() {
         String reauthenticateWithUser = TEAM_A_1_USERNAME
 
-        createTeamsInDatabase(ADMIN_USERNAME, ADMIN_EMAIL, ADMIN_ROLE, reauthenticateWithUser)
-
         Map assayData = (Map) remote.exec({
             //Create two assays
             SpringSecurityUtils.reauthenticate(reauthenticateWithUser, null)
-            String childLabel = "child"
-            String parentLabel = "parent"
-            Element childElement = Element.findByLabel(childLabel)
-            if (!childElement) {
-                childElement = Element.build(label: childLabel).save(flush: true)
-            }
-            Measure childMeasure = Measure.findByResultType(childElement)
-            if (!childMeasure) {
-                childMeasure = Measure.build(resultType: childElement).save(flush: true)
-            }
 
-            Element parentElement = Element.findByLabel(parentLabel)
-            if (!parentElement) {
-                parentElement = Element.build(label: parentLabel).save(flush: true)
-            }
-            Measure parentMeasure = Measure.findByResultType(parentElement)
-            if (!parentMeasure) {
-                parentMeasure = Measure.build(resultType: parentElement, childMeasures: [childMeasure] as Set).save(flush: true)
-            }
-            childMeasure.parentMeasure = parentMeasure
-            childMeasure.parentChildRelationship = HierarchyType.SUPPORTED_BY
-            childMeasure.save(flush: true)
+            Assay assay1 = Assay.build(assayName: "Assay Name10").save(flush: true)
 
-            Assay assay1 = Assay.build(assayName: "Assay Name10", measures: [childMeasure, parentMeasure] as Set).save(flush: true)
-            String experimentsAlias = "experiment1"
-            final Experiment experiment1 = Experiment.build(experimentName: experimentsAlias, assay: assay1, capPermissionService: null).save(flush: true)
-            ExternalReference.build(extAssayRef: "aid=2", experiment: experiment1).save(flush: true)
-
-            AssayContext.build(assay: assay1, contextName: "alpha1").save(flush: true)
-
-            Assay assay2 = Assay.build(assayName: "Assay Name10", measures: [childMeasure, parentMeasure] as Set).save(flush: true)
-
-            experimentsAlias = "experiment2"
-            final Experiment experiment2 = Experiment.build(experimentName: experimentsAlias, assay: assay2, capPermissionService: null).save(flush: true)
-            ExternalReference.build(extAssayRef: "aid=1", experiment: experiment2).save(flush: true)
-            AssayContext.build(assay: assay2, contextName: "alpha2").save(flush: true)
             //create assay context
-            return [assayId1: assay1.id, assayId2: assay2.id, experimentId1: experiment1.id, experimentId2: experiment2.id]
+            return [assayId1: assay1.id]
         })
-        experimentData.put("experimentId1", assayData.experimentId1)
-        experimentData.put("experimentId2", assayData.experimentId2)
         assayIdList.add(assayData.assayId1)
-        assayIdList.add(assayData.assayId2)
-
 
     }     // run before the first feature method
     def cleanupSpec() {
@@ -89,13 +48,6 @@ class MergeAssayDefintionControllerFunctionalSpec extends BardControllerFunction
         Sql sql = Sql.newInstance(dburl, dbusername,
                 dbpassword, driverClassName)
         sql.call("{call bard_context.set_username(?)}", [TEAM_A_1_USERNAME])
-
-        sql.execute("DELETE FROM EXTERNAL_REFERENCE WHERE EXPERIMENT_ID=${experimentData.experimentId1}")
-        sql.execute("DELETE FROM EXTERNAL_REFERENCE WHERE EXPERIMENT_ID=${experimentData.experimentId2}")
-        sql.execute("DELETE FROM EXPRMT_MEASURE WHERE EXPERIMENT_ID=${experimentData.experimentId1}")
-        sql.execute("DELETE FROM EXPRMT_MEASURE WHERE EXPERIMENT_ID=${experimentData.experimentId2}")
-        sql.execute("DELETE FROM EXPRMT_CONTEXT WHERE EXPERIMENT_ID=${experimentData.experimentId1}")
-        sql.execute("DELETE FROM EXPRMT_CONTEXT WHERE EXPERIMENT_ID=${experimentData.experimentId2}")
         for (Long assayId : assayIdList) {
             sql.execute("DELETE FROM EXPERIMENT WHERE ASSAY_ID=${assayId}")
             sql.execute("DELETE FROM ASSAY_CONTEXT WHERE ASSAY_ID=${assayId}")
@@ -136,38 +88,4 @@ class MergeAssayDefintionControllerFunctionalSpec extends BardControllerFunction
 
 
     }
-
-    def 'test show #desc'() {
-        given:
-        RESTClient client = getRestClient(controllerUrl, "show", team, teamPassword)
-
-        when:
-        final Response response = client.get()
-        then:
-        assert response.statusCode == expectedHttpResponse
-        where:
-        desc    | team           | teamPassword   | expectedHttpResponse
-        "ADMIN" | ADMIN_USERNAME | ADMIN_PASSWORD | HttpServletResponse.SC_OK
-
-    }
-
-    def 'test show - forbidden  #desc'() {
-        given:
-        RESTClient client = getRestClient(controllerUrl, "show", team, teamPassword)
-
-        when:
-        client.get()
-        then:
-        def ex = thrown(RESTClientException)
-        assert ex.response.statusCode == expectedHttpResponse
-        where:
-        desc       | team              | teamPassword      | expectedHttpResponse
-        "User A_1" | TEAM_A_1_USERNAME | TEAM_A_1_PASSWORD | HttpServletResponse.SC_FORBIDDEN
-        "User B"   | TEAM_B_1_USERNAME | TEAM_B_1_PASSWORD | HttpServletResponse.SC_FORBIDDEN
-        "User A_2" | TEAM_A_2_USERNAME | TEAM_A_2_PASSWORD | HttpServletResponse.SC_FORBIDDEN
-        "CURATOR"  | CURATOR_USERNAME  | CURATOR_PASSWORD  | HttpServletResponse.SC_FORBIDDEN
-
-
-    }
-
 }
