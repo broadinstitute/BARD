bfb160ea76fb51d943f95532f0693e8fa0011090
Dan Durkin <ddurkin@broadinstitute.org>
2013-10-09 13:03:22 -0400

    using constant instead of literal for referrer header

--- BARD/grails-app/controllers/bard/db/context/item/ContextItemController.groovy
+++ BARD/grails-app/controllers/bard/db/context/item/ContextItemController.groovy
@@ -15,9 +15,12 @@ import grails.plugins.springsecurity.Secured
 import grails.plugins.springsecurity.SpringSecurityService
 import grails.validation.Validateable
 import groovy.transform.InheritConstructors
-import org.springframework.security.access.AccessDeniedException
 
 import javax.servlet.http.HttpServletResponse
+
+import org.springframework.security.access.AccessDeniedException
+
+import static org.codehaus.groovy.grails.web.servlet.HttpHeaders.REFERER
 
 @Mixin(EditingHelper)
 @Secured(['isAuthenticated()'])
@@ -85,7 +88,7 @@ class ContextItemController {
 
         basicContextItemCommand.delete()
 
-        if(request.getHeader('referer')?.contains('/contextItem/')){
+        if(request.getHeader(REFERER)?.contains('/contextItem/')){
             redirect(action: 'create', params: params)
         }
         else{
