6b5daf97855b341db12dddc69cb8c3de8b7da1dc
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-10-11 23:58:34 -0400

    Fixing down time scheduler to poll every 10 minutes

--- BARD/web-app/js/cap/downtime.js
+++ BARD/web-app/js/cap/downtime.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-//    $("#downtimenotify").notify({
-//        speed: 500,
-//        expires: false
-//    });
+    $("#downtimenotify").notify({
+        speed: 500,
+        expires: false
+    });
     /*
      Register a grailsEvents handler for this window, constructor can take a root URL,
      a path to event-bus servlet and options. There are sensible defaults for each argument
@@ -21,7 +21,7 @@ $(document).ready(function () {
 //
 //        }
 //    });
-    poll();//poll every 10 minutes
+    execPoll();
     //make an ajax call to find the current scheduled downtime
 
 //    function buildDownTimeDiv(data) {
@@ -36,14 +36,20 @@ $(document).ready(function () {
 });
 //Poll every 10 minutes
 function poll() {
-    setTimeout(function () {
-        $.ajax({
-            type: 'GET',
-            url: '/BARD/downTimeScheduler/currentDownTimeInfo',
-            success: function (data) {
-                buildDownTimeDiv(data);
-            },
-            complete: poll
-        });
-    }, 600000);
+    setTimeout(execPoll, 600000);
+}
+function execPoll() {
+    $.ajax({
+        type: 'GET',
+        url: '/BARD/downTimeScheduler/currentDownTimeInfo',
+        success: function (data) {
+            if (data) {
+                $("#downtimenotify").notify("create", {
+                    title: "Down Time Notification!!",
+                    text: data
+                });
+            }
+        },
+        complete: poll
+    });
 }
