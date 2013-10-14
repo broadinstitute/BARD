f8cb9136963e7aaa81a7cd079e6e33f46be794aa
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-10-14 13:00:46 -0400

    Fixing issues with downtime scheduler.

--- BARD/web-app/js/cap/downtime.js
+++ BARD/web-app/js/cap/downtime.js
@@ -3,36 +3,8 @@ $(document).ready(function () {
         speed: 500,
         expires: false
     });
-    /*
-     Register a grailsEvents handler for this window, constructor can take a root URL,
-     a path to event-bus servlet and options. There are sensible defaults for each argument
-     */
-//    var grailsEvents = new grails.Events("/BARD");
-//    grailsEvents.on('downTime', function (data) {
-//        buildDownTimeDiv(data);
-//    });
-//
-//
-//    $.ajax({
-//        type: 'GET',
-//        url: '/BARD/downTimeScheduler/currentDownTimeInfo',
-//        success: function (data) {
-//            buildDownTimeDiv(data);
-//
-//        }
-//    });
     execPoll();
-    //make an ajax call to find the current scheduled downtime
-
-//    function buildDownTimeDiv(data) {
-//        if (data) {
-//            $("#downtimenotify").notify("create", {
-//                title: "Down Time Notification!!",
-//                text: data
-//            });
-//        }
-//    }
-
+    poll();
 });
 //Poll every 10 minutes
 function poll() {
