b107849f226cb76d51e390b4d77dd73c337e4db3
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-10-16 19:11:43 -0400

    Fixing broken build

--- BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
@@ -5,11 +5,17 @@ databaseChangeLog = {
     changeSet(author: "jasiedu", id: "iteration-035/01-remove-new-object-role", dbms: "oracle", context: "standard") {
         sqlFile(path: "iteration_035/01-remove-new-object-role.sql", stripComments: true)
     }
+//    changeSet(author: "jasiedu", id: "iteration-035/00-drop-registration-tables", dbms: "oracle", context: "standard") {
+//        sqlFile(path: "iteration_035/00-drop-registration-tables.sql", stripComments: true)
+//    }
     changeSet(author: "jasiedu", id: "iteration-035/02-reset-auditing-on-Person-Table", dbms: "oracle", context: "standard") {
         grailsChange {
             change {
-                sql.call('''call auditing_setup.update_settings('', 'Refresh')''')
-                sql.call('''call auditing_init.make_triggers()''')
+                def row = sql.firstRow("SELECT OBJECT_NAME FROM ALL_PROCEDURES WHERE  OBJECT_NAME = 'MANAGE_NAMES'")
+                if(row?.OBJECT_NAME){//only execute this if the package already exist. This would happen if you copy data from prod for instance
+                    sql.call('''call auditing_setup.update_settings('', 'Refresh')''')
+                    sql.call('''call auditing_init.make_triggers()''')
+                }
             }
         }
     }
