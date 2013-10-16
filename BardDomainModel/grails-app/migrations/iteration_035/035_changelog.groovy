a9398bd001a11c00c69c9d5ca29e53bdcad00d18
Jacob Asiedu <jasiedu@broadinstitute.org>
2013-10-16 17:01:52 -0400

    Adding a script to use email addresses for usernames, only for users with valid email addresses

--- BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
@@ -5,4 +5,30 @@ databaseChangeLog = {
     changeSet(author: "jasiedu", id: "iteration-035/01-remove-new-object-role", dbms: "oracle", context: "standard") {
         sqlFile(path: "iteration_035/01-remove-new-object-role.sql", stripComments: true)
     }
+    changeSet(author: "jasiedu", id: "iteration-035/02-reset-auditing-on-Person-Table", dbms: "oracle", context: "standard") {
+        grailsChange {
+            change {
+                sql.call('''call auditing_setup.update_settings('', 'Refresh')''')
+                sql.call('''call auditing_init.make_triggers()''')
+            }
+        }
+    }
+    changeSet(author: 'jasiedu', id: 'update user names with email address', dbms: 'oracle', context: 'standard') {
+        grailsChange {
+            change {
+                sql.execute("""BEGIN
+                               bard_context.set_username('jasiedu');
+                               END;
+                               """)
+                String query = """SELECT EMAIL_ADDRESS
+                                  FROM PERSON
+                                  WHERE EMAIL_ADDRESS IS NOT NULL"""
+                sql.eachRow(query) { row ->
+                    String updateStatement = "UPDATE PERSON SET USERNAME='${row.EMAIL_ADDRESS}' WHERE EMAIL_ADDRESS='${row.EMAIL_ADDRESS}'"
+                    //println("${updateStatement}")
+                    sql.executeUpdate(updateStatement)
+                }
+            }
+        }
+    }
 }
