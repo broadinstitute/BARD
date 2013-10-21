754043c8d8c234bfbb51753e9c7c8dfaa8102e7a
Gil Walzer <gil.walzer@broadinstitute.org>
2013-10-21 11:28:31 +0300

    https://www.pivotaltracker.com/story/show/58992294: Remove the Replaced_By_ID column in Element

--- BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_035/035_changelog.groovy
@@ -5,4 +5,8 @@ databaseChangeLog = {
     changeSet(author: "jasiedu", id: "iteration-035/01-remove-new-object-role", dbms: "oracle", context: "standard") {
         sqlFile(path: "iteration_035/01-remove-new-object-role.sql", stripComments: true)
     }
+
+    changeSet(author: "gwalzer", id: "iteration-035/02-drop-Replaced-By-Id-column-in-Element-table", dbms: "oracle", context: "standard") {
+        dropColumn(columnName: "REPLACED_BY_ID", tableName: "ELEMENT")
+    }
 }
