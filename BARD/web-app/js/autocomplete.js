c314a587356168e2edbfe31540b5deba92d93a2f
Philip Montgomery <pmontgom@broadinstitute.org>
2013-10-31 16:17:31 -0400

    Fix highlighting of suggested terms on home page
    [Fixes #59804670]
    
    Used the approach from
    http://stackoverflow.com/questions/3488016/using-html-in-jquery-ui-autocomplete/8218916#8218916

--- BARD/web-app/js/autocomplete.js
+++ BARD/web-app/js/autocomplete.js
@@ -4,15 +4,22 @@ $(document).ready(function () {
     var autoOpts = {
         source:"/BARD/bardWebInterface/autoCompleteAssayNames",
         minLength:2,
-        html: true,
         delay:1000
     };
 
-    $("#searchString").autocomplete(autoOpts);
+    $("#searchString").autocomplete(autoOpts)
+        .data( "autocomplete" )._renderItem = function( ul, item ) {
+            return $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append( "<a>"+ item.label + "</a>" )
+                .appendTo( ul );
+        };
+
     $("#searchString").bind("autocompleteselect", function (event, ui) {
         $("#searchString").val(ui.item.value)
         $("#searchButton").click();
     });
+
     // make sure to close the autocomplete box when the search button or ENTER are clicked
     $("#searchButton").click(function () {
         $("#searchString").autocomplete("close");
