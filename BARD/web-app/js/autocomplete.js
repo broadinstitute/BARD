de5ed944166108f45037630cba52b3e32680f31f
Philip Montgomery <pmontgom@broadinstitute.org>
2013-11-01 14:23:49 -0400

    Fixed highlighting of suggested terms on home page a different way.
    The issue why it worked on some pages but not others was home page had 2 jquery libs loaded which broke it.
    
    This reverts commit c314a587356168e2edbfe31540b5deba92d93a2f.

--- BARD/web-app/js/autocomplete.js
+++ BARD/web-app/js/autocomplete.js
@@ -4,22 +4,15 @@ $(document).ready(function () {
     var autoOpts = {
         source:"/BARD/bardWebInterface/autoCompleteAssayNames",
         minLength:2,
+        html: true,
         delay:1000
     };
 
-    $("#searchString").autocomplete(autoOpts)
-        .data( "autocomplete" )._renderItem = function( ul, item ) {
-            return $( "<li></li>" )
-                .data( "item.autocomplete", item )
-                .append( "<a>"+ item.label + "</a>" )
-                .appendTo( ul );
-        };
-
+    $("#searchString").autocomplete(autoOpts);
     $("#searchString").bind("autocompleteselect", function (event, ui) {
         $("#searchString").val(ui.item.value)
         $("#searchButton").click();
     });
-
     // make sure to close the autocomplete box when the search button or ENTER are clicked
     $("#searchButton").click(function () {
         $("#searchString").autocomplete("close");
