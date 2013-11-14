2432235c9f1e6c80983b7bb4096b8c3172710949
Ben R Alexander <balexand@broadinstitute.org>
2013-11-14 12:58:56 +0000

    Extend the molecular spreadsheet to handle nonnumeric values.   pivotal=60516034

--- BARD/grails-app/domain/molspreadsheet/HillCurveValueHolder.groovy
+++ BARD/grails-app/domain/molspreadsheet/HillCurveValueHolder.groovy
@@ -18,12 +18,13 @@ class HillCurveValueHolder {
     java.util.List<java.lang.Double> conc = []
     java.util.List<java.lang.Double> response = []
     String qualifier = ""
+    String stringValue = "" // some cells are non numeric.  Store those values here
     String xAxisLabel = ''
     String yAxisLabel = ''
     static mapping = {
         table('MOL_SS_HILL_CURVE_VALUE_HOLDER')
         id(generator: 'sequence', params: [sequence: 'HILL_CURVE_VALUE_HOLDER_ID_SEQ'])
-        spreadSheetActivityStorage( column: "MOL_SS_ACTIVITY_STORAGE_ID")
+        spreadSheetActivityStorage(column: "MOL_SS_ACTIVITY_STORAGE_ID")
     }
     static constraints = {
         identifier nullable: false
@@ -38,21 +39,21 @@ class HillCurveValueHolder {
 
 
     void setIdentifier(String identifier) {
-        this.identifier =  identifier
+        this.identifier = identifier
     }
 
 
 
-    void setQualifier (MolSpreadSheetCellType molSpreadSheetCellType)  {
-        if (molSpreadSheetCellType == MolSpreadSheetCellType.greaterThanNumeric)  {
+    void setQualifier(MolSpreadSheetCellType molSpreadSheetCellType) {
+        if (molSpreadSheetCellType == MolSpreadSheetCellType.greaterThanNumeric) {
             qualifier = ">"
-        }  else if (molSpreadSheetCellType == MolSpreadSheetCellType.lessThanNumeric)  {
+        } else if (molSpreadSheetCellType == MolSpreadSheetCellType.lessThanNumeric) {
             qualifier = "<"
         }
     }
 
 
-    void setQualifier (String qualifier)  {
+    void setQualifier(String qualifier) {
         this.qualifier = qualifier
     }
 
@@ -60,32 +61,41 @@ class HillCurveValueHolder {
     @Override
     public String toString() {
         String returnValue = "Missing data qualifier"
+        Boolean preferAStringValue = false
 
+        // Decide which datatype we are dealing with
         Double numericalReturnValue = Double.NaN
-        if (slope  != null)   {
+        if (stringValue != "") {   // if we have a string value then use it
+            returnValue = stringValue
+            preferAStringValue = true
+        } else if (slope != null) {
             numericalReturnValue = slope
-        }
-        else if ((response  != null) &&
-                (response.size()  == 1)  &&
-                (response[0]  != null)) {
+        } else if ((response != null) &&
+                (response.size() == 1) &&
+                (response[0] != null)) {
             numericalReturnValue = response[0]
-        } else  {
+        } else  {        // this shouldn't happen
             //TODO: find the business rule describing desired actions under these circumstances
-            log.info "Problem identified by HillCurveValueHolder: no slope and as well no single valued response"
+            log.info "Problem identified by HillCurveValueHolder: no slope, no string, and therefore no single valued response"
         }
 
-        if (numericalReturnValue != Double.NaN)  {
-            returnValue  = new ExperimentalValueUtil(numericalReturnValue,ExperimentalValueUnitUtil.Micromolar, ExperimentalValueUnitUtil.Micromolar)
-        } else {
-            returnValue  = '--'
+        // if we have a numerical value we have a little more work to do
+        if (!preferAStringValue) {
+
+            // Use an appropriate precision as long as the number is non-null
+            if (numericalReturnValue != Double.NaN) {
+                returnValue = new ExperimentalValueUtil(numericalReturnValue, ExperimentalValueUnitUtil.Micromolar, ExperimentalValueUnitUtil.Micromolar)
+            } else {
+                returnValue = '--'
+            }
+
+            // add a qualifier if we have one ( otherwise append a null string ).
+            if (!returnValue.contains("--")) {
+                returnValue = qualifier + " " + returnValue
+            }
         }
 
-        if (returnValue.contains("--")) {
-            return returnValue
-        }
-        else {
-            return qualifier+" "+returnValue
-        }
+        returnValue
     }
 
 
