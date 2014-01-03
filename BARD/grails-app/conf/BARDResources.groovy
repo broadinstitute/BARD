6f3e3ef7df2d4feec5dd85041d1eca67b6bdbfc9
Jacob Asiedu <jasiedu@broadinstitute.org>
2014-01-03 16:48:09 -0500

    Fixing issue with the system not finding history’s file when it is “war” deployed. Changed the path to the folder

--- BARD/grails-app/conf/BARDResources.groovy
+++ BARD/grails-app/conf/BARDResources.groovy
@@ -176,12 +176,8 @@ modules = {
     }
 
     historyJsHtml5 {
-//        history.js commit a6e2554cd5e10bd07f162bb876b0bbf48514b900
-
-        resource url: "js/history.js-master-commit-a6e2554/scripts/bundled-uncompressed/html4+html5/jquery.history.js"
-
+        resource url: "js/historyjs/jquery.history.js"
     }
-
     //I
     idSearch {
         resource url: "js/idSearchDialog.js"
