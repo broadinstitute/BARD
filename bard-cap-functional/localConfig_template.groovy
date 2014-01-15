b9a598d8ca704ef39d88f9c5c3a387afbe170930
Dan Durkin <ddurkin@broadinstitute.org>
2014-01-15 14:53:05 -0500

    added gradle structure and a localConfig_template and rule for git to ignore a localConfig.groovy file for local properties

---
+++ bard-cap-functional/localConfig_template.groovy
@@ -0,0 +1,8 @@
+mockUsers {
+	integrationTestUser {
+		roles = ['ROLE_USER', 'ROLE_CURATOR', 'ROLE_BARD_ADMINISTRATOR']
+		username = 'someUser'
+		password = 'password'
+		email = 'someUser@nowhere.com'
+	}
+}
