0e7f6fa07e65835eab3e738f3d98a3094a6f9e7f
Dan Durkin <ddurkin@broadinstitute.org>
2014-01-15 14:03:11 -0500

    adde config and dependencies for phantomjs

--- bard-cap-functional/test/functional/base/GebConfig.groovy
+++ bard-cap-functional/test/functional/base/GebConfig.groovy
@@ -3,19 +3,22 @@
  See: http://www.gebish.org/manual/current/configuration.html
  */
 
+
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.SystemUtils
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriverService
 import org.openqa.selenium.remote.CapabilityType
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
 driver = {
-    def d = new HtmlUnitDriver()
-    d.javascriptEnabled = true
-    d
+    new PhantomJSDriver(getPhantomjsDesiredCapabilities())
 }
 atCheckWaiting = true
 baseUrl = System.properties.getProperty('server.url') ?: "http://localhost:8080/BARD/"
@@ -31,13 +34,7 @@ environments {
     }
 
     firefox {
-        File profileDir = new File("target/firefoxProfile")
-        if (!profileDir.exists()) {
-            profileDir.mkdir()
-        }
-        FirefoxProfile profile = new FirefoxProfile(profileDir)
-        profile.setPreference("webdriver.load.strategy", "unstable");
-        profile.setAssumeUntrustedCertificateIssuer(true);
+        getFirefoxProfile()
         driver = { new FirefoxDriver() }
     }
 
@@ -49,18 +46,42 @@ environments {
         driver = { new InternetExplorerDriver(capabilities) }
     }
     remote {
-        File profileDir = new File("target/firefoxProfile")
-        if (!profileDir.exists()) {
-            profileDir.mkdir()
-        }
-        FirefoxProfile profile = new FirefoxProfile(profileDir)
-        profile.setPreference("webdriver.load.strategy", "unstable");
-        profile.setAssumeUntrustedCertificateIssuer(true);
-
-
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability(FirefoxDriver.PROFILE, profile)
-        driver = { new RemoteWebDriver(capabilities) }
+        driver = { new RemoteWebDriver(getPhantomjsDesiredCapabilities()) }
+    }
+    phantomjs {
+        driver = {new PhantomJSDriver(getPhantomjsDesiredCapabilities())}
     }
 
+}
+
+private DesiredCapabilities getPhantomjsDesiredCapabilities() {
+    final List<String> cliArgs = ["--web-security=false", "--ssl-protocol=any", "--ignore-ssl-errors=true", "--webdriver-loglevel=INFO"]
+    final DesiredCapabilities desiredCapabilities = DesiredCapabilities.phantomjs()
+    desiredCapabilities.setJavascriptEnabled(true)
+    desiredCapabilities.setCapability("takesScreenshot", true)
+    desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgs)
+    desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getPhantomJsExecutablePath())
+    desiredCapabilities
+}
+
+public static String getPhantomJsExecutablePath() {
+    if (StringUtils.isNotBlank(System.getProperty("PHANTOM_JS_EXECUTABLE_PATH"))) {
+        return System.getProperty("PHANTOM_JS_EXECUTABLE_PATH");
+    } else if (SystemUtils.IS_OS_MAC_OSX) {
+        return "/usr/local/bin/phantomjs"   // assuming brew was used to install
+    } else if (SystemUtils.IS_OS_LINUX) {
+        return "/cbplat/bard/dev_tools/phantomjs/phantomjs-1.9.2-linux-x86_64/bin"
+    } else {
+        throw new RuntimeException("Unsure of OS, so couldn't pick an executable for phantomjs")
+    }
+}
+
+private void getFirefoxProfile() {
+    File profileDir = new File("target/firefoxProfile")
+    if (!profileDir.exists()) {
+        profileDir.mkdir()
+    }
+    FirefoxProfile profile = new FirefoxProfile(profileDir)
+    profile.setPreference("webdriver.load.strategy", "unstable")
+    profile.setAssumeUntrustedCertificateIssuer(true)
 }
