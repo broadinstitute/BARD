18e3f445e9991ec4a209e7900e39146730431457
Philip Montgomery <pmontgom@broadinstitute.org>
2014-01-16 15:44:18 -0500

    fixed syntax of reference load script

--- BardDomainModel/grails-app/migrations/load-reference-data.groovy
+++ BardDomainModel/grails-app/migrations/load-reference-data.groovy
@@ -1,9 +1,7 @@
 databaseChangeLog = {
 	changeSet(author: 'pmontgom', id: 'populate-reference-data.sql', dbms: 'oracle', context: 'load-reference', runOnChange: 'false') {
-		grailsChange {
-			sqlFile(path: "sql/populate-reference-data.sql");
-			sqlFile(path: "sql/populate-reference-element.sql");
-		}
+        sqlFile(path: "sql/populate-reference-data.sql");
+        sqlFile(path: "sql/populate-reference-element.sql");
 	}
 }
 
