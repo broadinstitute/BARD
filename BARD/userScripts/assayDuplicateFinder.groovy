d2e757af3149d168476810fd6936cf1fc11ec6d8
dllahr <dlahr@broadinstitute.org>
2014-04-15 06:02:24 -0400

    make assay traits for comparison more abstract to allow comparison across more element types

--- BARD/userScripts/assayDuplicateFinder.groovy
+++ BARD/userScripts/assayDuplicateFinder.groovy
@@ -11,6 +11,10 @@ import bard.db.registration.AssayContextItem
 final Element targetCell = Element.findById(369)
 final Element atccCellName = Element.findById(1952)
 final Element otherCellName = Element.findById(377)
+final Element culturedCell = Element.findById(244)
+
+final AssayTraitsComparator atComp = new AssayTraitsComparator([atccCellName, otherCellName, culturedCell] as List<Element>)
+
 
 println("retrieving assay context items with target cell from database:")
 List<AssayContextItem> aciList = AssayContextItem.findAllByValueElement(targetCell)
@@ -24,20 +28,25 @@ for (AssayContextItem aci : aciList) {
     AssayContext ac = aci.context
 
     AssayTraits at = null
-    boolean found = false
     for (AssayContextItem otherAci : ac.contextItems) {
-        if (! found && otherAci.attributeElementId.equals(atccCellName.id)) {
-            at = new AssayTraits(ac.assay, atccCellName.id, otherAci.extValueId, null)
-            found = true
-        } else if (! found && otherAci.attributeElementId.equals(otherCellName.id)) {
-            at = new AssayTraits(ac.assay, otherCellName.id, null, otherAci.valueElementId)
-            found = true
-        } else if (found && (otherAci.attributeElementId.equals(atccCellName.id) || otherAci.attributeElementId.equals(otherCellName.id))) {
-            println("multiple cell line specifications found:  assay.id:${ac.assay.id} ac.id:${ac.id}")
+
+        Iterator<Element> cellAttrIter = atComp.attrCompList.iterator()
+        while (cellAttrIter.hasNext()) {
+            Element cellAttr = cellAttrIter.next()
+
+            if (otherAci.attributeElementId == cellAttr.id) {
+                if (! at) {
+                    Map<Element, AssayContextItem> map = new HashMap<>()
+                    map.put(cellAttr, otherAci)
+                    at = new AssayTraits(ac.assay, map, atComp.attrCompList)
+                } else {
+                    println("multiple cell line specifications found:  assay.id:${ac.assay.id} ac.id:${ac.id}")
+                }
+            }
         }
     }
 
-    if (found) {
+    if (at) {
         List<AssayTraits> list = assayTraitsMap.get(at)
         if (! list) {
             list = new LinkedList<>()
@@ -54,7 +63,6 @@ for (AssayContextItem aci : aciList) {
     }
 }
 
-final AssayTraitsComparator atComp = new AssayTraitsComparator()
 
 List<List<AssayTraits>> atListList = new ArrayList<>(assayTraitsMap.values())
 atListList = atListList.findAll({List<AssayTraits> it -> it.size() > 1})
@@ -82,28 +90,33 @@ for (List<AssayTraits> list : atListList) {
 return
 
 class AssayTraits {
+    final List<Element> attrList
+
     final Assay assay
 
-    final Long cellSourceId
-    final String atccId
-    final Long otherCellNameId
+    Map<Element, AssayContextItem> attrValueMap
 
-    AssayTraits(Assay assay, Long cellSourceId, String atccId, Long otherCellNameId) {
+    AssayTraits(Assay assay, Map<Element, AssayContextItem> attrValueMap, List<Element> attrList) {
         this.assay = assay
-        this.cellSourceId = cellSourceId
-        this.atccId = atccId
-        this.otherCellNameId = otherCellNameId
 
-        if ((atccId != null && otherCellNameId != null) || (null == atccId && null == otherCellNameId)) {
-            throw new RuntimeException("AssayTraits constructor - one and only one of atccId and otherCellNameId must be non null")
-        }
+        this.attrValueMap = Collections.unmodifiableMap(attrValueMap)
+
+        this.attrList = attrList
     }
 
     @Override
     int hashCode() {
-        int result = cellSourceId.hashCode()
-        result = (31*result) + (atccId ? atccId.hashCode() : 0)
-        result = (31*result) + (otherCellNameId ? otherCellNameId.hashCode() : 0)
+        int result = 0
+        for (Element attribute : attrList) {
+            if (attrValueMap.containsKey(attribute)) {
+                result = (31*result) + attribute.id.hashCode()
+
+                AssayContextItem aci = attrValueMap.get(attribute)
+                int aciHashCode = aci.valueElementId ? aci.valueElementId.hashCode() : aci.extValueId.hashCode()
+
+                result = (31*result) + aciHashCode
+            }
+        }
         return result
     }
 
@@ -112,11 +125,27 @@ class AssayTraits {
         if (obj instanceof AssayTraits) {
             AssayTraits other = (AssayTraits)obj
 
-            boolean result = cellSourceId == other.cellSourceId
-            result = result && (atccId == null && other.atccId == null) || atccId.equals(other.atccId)
-            result = result && (otherCellNameId == other.otherCellNameId)
+            if (attrValueMap.keySet().size() != other.attrValueMap.keySet().size()) {
+                return false
+            } else {
+                for (Element attribute : attrValueMap.keySet()) {
+                    if (! other.attrValueMap.containsKey(attribute)) {
+                        return false
+                    }
 
-            return result
+                    AssayContextItem aci = attrValueMap.get(attribute)
+                    AssayContextItem otherAci = other.attrValueMap.get(attribute)
+
+                    if (aci.valueElementId && otherAci.valueElementId && aci.valueElementId != otherAci.valueElementId) {
+                        return false
+                    }
+                    if (aci.extValueId && otherAci.extValueId && ! aci.extValueId.equals(otherAci.extValueId)) {
+                        return false
+                    }
+                }
+
+                return true
+            }
         } else {
             return false
         }
@@ -124,31 +153,51 @@ class AssayTraits {
 
     @Override
     String toString() {
-        String cellIdentifier = atccId != null ? "atccId:$atccId" : "otherCellNameId:$otherCellNameId"
-        return "assay.id:${assay.id} $cellIdentifier".toString()
+        StringBuilder builder = new StringBuilder()
+        builder.append("assay.id:${assay.id} ")
+
+        for (Element attr : attrValueMap.keySet()) {
+            AssayContextItem aci = attrValueMap.get(attr)
+            String v = aci.valueElement ? aci.valueElement.label : aci.extValueId
+
+            builder.append("${attr.label}:$v ")
+        }
+        return builder.toString()
     }
 }
 
 
 class AssayTraitsComparator implements Comparator<AssayTraits> {
+    final List<Element> attrCompList
+
+    AssayTraitsComparator(List<Element> attrCompList) {
+        this.attrCompList = Collections.unmodifiableList(attrCompList)
+    }
+
     @Override
     int compare(AssayTraits o1, AssayTraits o2) {
-        if (o1.cellSourceId == o2.cellSourceId) {
-            if (o1.atccId) {
-                if (o1.atccId.equals(o2.atccId)) {
-                    return o1.assay.id - o2.assay.id
-                } else {
-                    return o1.atccId.compareTo(o2.atccId)
+
+        for (Element attr : attrCompList) {
+            if (o1.attrValueMap.containsKey(attr) && o2.attrValueMap.containsKey(attr)) {
+                AssayContextItem v1 = o1.attrValueMap.get(attr)
+                AssayContextItem v2 = o2.attrValueMap.get(attr)
+
+                if (v1.attributeElement && v2.attributeElement && v1.attributeElementId != v2.attributeElementId) {
+                    return v1.attributeElementId - v2.attributeElementId
+                }
+                if (v1.extValueId && v2.extValueId && ! v1.extValueId.equals(v2.extValueId)) {
+                    return v1.extValueId.compareTo(v2.extValueId)
                 }
             } else {
-                if (o1.otherCellNameId == o2.otherCellNameId) {
-                    return o1.assay.id - o2.assay.id
-                } else {
-                    return o1.otherCellNameId - o2.otherCellNameId
+                int a1 = o1.attrValueMap.containsKey(attr) ? attr.id : -1
+                int a2 = o2.attrValueMap.containsKey(attr) ? attr.id : -1
+
+                if (a1 != a2) {
+                    return a1 - a2
                 }
             }
-        } else {
-            return o1.cellSourceId - o2.cellSourceId
         }
+
+        return o1.assay.id - o2.assay.id
     }
 }
