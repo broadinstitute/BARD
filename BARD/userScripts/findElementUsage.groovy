e5f0f2d44dcf73d1dad65b5f85f732b287a1fd4f
dllahr <dlahr@broadinstitute.org>
2014-04-24 05:59:03 -0400

    add option to exclude found contexts that are part of retired items (assays, projects, experiments).  true by default

--- BARD/userScripts/findElementUsage.groovy
+++ BARD/userScripts/findElementUsage.groovy
@@ -1,4 +1,9 @@
+// @param label excludeRetired
+
+// @description find all contexts (assay, project, experiment) where an element appears.  excludeRetired defaults to true
+
 import bard.db.dictionary.Element
+import bard.db.enums.Status
 import bard.db.experiment.ExperimentContext
 import bard.db.experiment.ExperimentContextItem
 import bard.db.model.AbstractContext
@@ -12,12 +17,11 @@ import bard.db.registration.AssayContextItem
  * Created by dlahr on 3/27/2014.
  */
 
-final String label = "biology"
 
 List<Element> foundList = Element.findAllByLabelIlike(label)
 
 if (foundList.size() == 0) {
-    println("no matching element found")
+    println("no matching element found for entered label:  $label")
     return
 } else if (foundList.size() > 1) {
     println("DANGER:  multiple elements found that only differ in there capitalization.  Fix this!")
@@ -27,12 +31,14 @@ if (foundList.size() == 0) {
     return
 }
 
+final boolean excludeRetiredBool = excludeRetired.trim().equalsIgnoreCase("true")
+
 Element found = foundList.get(0)
 
 List<AssayContextItem> aciList = AssayContextItem.findAllByValueElement(found)
-List<AbstractContext> acList = prepareUniqueOrderedList(aciList)
+List<AbstractContext> acList = prepareUniqueOrderedList(aciList, excludeRetiredBool)
 aciList = AssayContextItem.findAllByAttributeElement(found)
-acList.addAll(prepareUniqueOrderedList(aciList))
+acList.addAll(prepareUniqueOrderedList(aciList, excludeRetiredBool))
 if (acList.size() > 0) {
     println("found in assay definitions:")
     displayAbstractContexts(acList, "assay")
@@ -43,7 +49,7 @@ List<ProjectContextItem> pciList = ProjectContextItem.findAllByValueElement(foun
 pciList.addAll(ProjectContextItem.findAllByAttributeElement(found))
 if (pciList.size() > 0) {
     println("found in projects:")
-    List<AbstractContext> pcList = prepareUniqueOrderedList(pciList)
+    List<AbstractContext> pcList = prepareUniqueOrderedList(pciList, excludeRetiredBool)
     displayAbstractContexts(pcList, "project")
 }
 
@@ -52,7 +58,7 @@ List<ExperimentContextItem> eciList = ExperimentContextItem.findAllByValueElemen
 eciList.addAll(ExperimentContextItem.findAllByAttributeElement(found))
 if (eciList.size() > 0) {
     println("found in experiments:")
-    List<AbstractContext> ecList = prepareUniqueOrderedList(eciList)
+    List<AbstractContext> ecList = prepareUniqueOrderedList(eciList, excludeRetiredBool)
     displayAbstractContexts(ecList, "experiment")
 }
 
@@ -67,6 +73,18 @@ class Utilities {
             return ac.project.id
         } else if (ac instanceof ExperimentContext) {
             return ac.experiment.id
+        } else {
+            return null
+        }
+    }
+
+    static Status getStatus(AbstractContext ac) {
+        if (ac instanceof AssayContext) {
+            return ac.assay.assayStatus
+        } else if (ac instanceof ProjectContext) {
+            return ac.project.projectStatus
+        } else if (ac instanceof ExperimentContext) {
+            return ac.experiment.experimentStatus
         } else {
             return null
         }
@@ -98,10 +116,16 @@ void displayAbstractContexts(Collection<AbstractContext> acColl, String type) {
     }
 }
 
-List<AbstractContext> prepareUniqueOrderedList(List<AbstractContextItem> aciList) {
-    List<AbstractContext> acList = aciList.collect({AbstractContextItem it -> return it.context})
+List<AbstractContext> prepareUniqueOrderedList(List<AbstractContextItem> aciList, boolean excludeRetired) {
+    List<AbstractContext> acList = aciList.collect({AbstractContextItem it ->
+        if (! excludeRetired || Utilities.getStatus(it.context) != Status.RETIRED) {
+            return it.context
+        }
+        return null
+    })
 
     Set<AbstractContext> acValueSet = new HashSet<>(acList)
+    acValueSet.remove(null)
 
     acList = new ArrayList<>(acValueSet)
 
