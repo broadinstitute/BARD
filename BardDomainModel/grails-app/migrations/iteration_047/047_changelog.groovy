7ca0b8e6875a211baa3e6d0e156ed38a0b52cbf6
Yasel Cruz <ycruz@scripps.edu>
2014-05-02 12:05:50 -0400

    Add Unique index to ASSAY_CTXT_EXP_MEASURE [#69317194]

--- BardDomainModel/grails-app/migrations/iteration_047/047_changelog.groovy
+++ BardDomainModel/grails-app/migrations/iteration_047/047_changelog.groovy
@@ -37,4 +37,41 @@ databaseChangeLog = {
             }
         }
     }
+
+    changeSet(author: "ycruz", id: "iteration-047/01-add-Unique-index-to-ASSAY_CTXT_EXP_MEASURE", dbms: "oracle", context: "standard") {
+        grailsChange {
+            change {
+                sql.execute("BEGIN bard_context.set_username('ycruz'); end;")
+
+                String DUPLICATES_QUERY = """select experiment_measure_id, assay_context_id, count(*) from ASSAY_CTXT_EXP_MEASURE group by experiment_measure_id, assay_context_id having count(*) > 1"""
+                String DUPLICATE_RECORDS = """select * from ASSAY_CTXT_EXP_MEASURE where experiment_measure_id = ? and  assay_context_id = ?"""
+                String delete_duplicate = """delete from ASSAY_CTXT_EXP_MEASURE where ASSAY_CTXT_EXP_MEASURE_ID = ?"""
+
+                def rows = sql.rows(DUPLICATES_QUERY)
+                if(rows.size() > 0){
+                    println("Delete duplicates ASSAY_CTXT_EXP_MEASURE. Size: " + rows.size())
+                    for(row in rows){
+                        def exp_measure_id = row.EXPERIMENT_MEASURE_ID
+                        def assay_context_id = row.ASSAY_CONTEXT_ID
+                        println("Duplicate EXPERIMENT_MEASURE_ID = ${exp_measure_id}, ASSAY_CONTEXT_ID = ${assay_context_id}")
+                        def duplicateRows = sql.rows(DUPLICATE_RECORDS, [exp_measure_id, assay_context_id])
+                        for(int i = duplicateRows.size(); i > 1; i--){
+                            def index = i - 1
+                            def dupRow = duplicateRows.get(index)
+                            def ASSAY_CTXT_EXP_MEASURE_ID = dupRow.ASSAY_CTXT_EXP_MEASURE_ID
+                            println("Deleting at index ${index}: ASSAY_CTXT_EXP_MEASURE_ID = ${ASSAY_CTXT_EXP_MEASURE_ID}")
+                            sql.execute(delete_duplicate, [ASSAY_CTXT_EXP_MEASURE_ID])
+                            println("Succesfully deleted duplicate ASSAY_CTXT_EXP_MEASURE with ASSAY_CTXT_EXP_MEASURE_ID = ${ASSAY_CTXT_EXP_MEASURE_ID}")
+                        }
+                    }
+                }
+                else{
+                    println("No duplicates ASSAY_CTXT_EXP_MEASUREs found. Size: ") + rows.size()
+                }
+
+                sql.execute("CREATE UNIQUE INDEX ak_assay_ctxt_exp_measure ON assay_ctxt_exp_measure(experiment_measure_id, assay_context_id)")
+                println("Finished adding Unique index to ASSAY_CTXT_EXP_MEASURE")
+            }
+        }
+    }
 }
